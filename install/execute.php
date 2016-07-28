ed425ce2b102fda689e457fdbcdf442a1ff96345
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-07-28 12:57:14 +0700

    Update execute.php
    bug fix db create

--- install/execute.php
+++ install/execute.php
@@ -6,16 +6,13 @@ $post = $_POST;
 $conf = load_env();
 
 if($type=='save_db') {
-	$con = mysqli_connect($post['DB_HOST'],$post['DB_USERNAME'],$post['DB_PASSWORD']);
+	$con = mysqli_connect($post['DB_HOST'],$post['DB_USERNAME'],$post['DB_PASSWORD'],$post['DB_DATABASE']);
 
-	// Check connection
+	// Check connection	
 	if (mysqli_connect_errno())
-	{
-		echo 0;
-		exit;
-	}	
-	//CREATE DATABASE IF NOT EXISTS DBName;
-	@mysqli_query($con,"CREATE DATABASE IF NOT EXISTS `$post[DB_DATABASE]`");
+	  {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  }	
 	
     $conf['DB_HOST'] = $post['DB_HOST'];
     $conf['DB_DATABASE'] = $post['DB_DATABASE'];
@@ -27,8 +24,16 @@ if($type=='save_db') {
     	$env .= "$k=$v\n";
     }
 
-    file_put_contents("../.env", $env);
-    echo 1;
+    if(is_writable("../.env")) {
+    	if(file_put_contents("../.env", $env)) {
+    		echo "success";
+    	}else{
+    		echo "Can't write setting to .env!";
+    	}
+
+    }else{
+    	echo "Please make sure file .env is writable!";
+    }    
 }elseif ($type=='check_table') {
 
 	$con = mysqli_connect($conf['DB_HOST'],$conf['DB_USERNAME'],$conf['DB_PASSWORD'],$conf['DB_DATABASE']);
