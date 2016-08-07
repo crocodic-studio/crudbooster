fffdf7d1e00f6a4a39cb4c5869f40c97bcd63848
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-08-07 18:19:20 +0700

    improve welcome page

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,15 +1,6 @@
 			
 
 
-
-			<div class='bg-copy'>
-				<div class='container'>
-					<div class='content'>
-						&copy; Copyright <?php echo date('Y');?>. All Right Reserved.
-					</div>
-				</div>
-			</div>
-
 		</div><!--END CONTAINER-->
 	</body>
 </html>
