7d0183b9f46abb9e8a1bdc555be6d1f36dd5395f
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-08-08 20:19:57 +0700

    re arrange form in module

--- app/Http/Controllers/ControllerMaster/ModulsController.php
+++ app/Http/Controllers/ControllerMaster/ModulsController.php
@@ -25,16 +25,15 @@ class ModulsController extends Controller {
 		$this->col = array();
 		$this->col[] = array('label'=>'Sorting','field'=>'sorting');
 		$this->col[] = array("label"=>"Name","field"=>"name");
-		$this->col[] = array("label"=>"Path","field"=>"path"); 
-		$this->col[] = array("label"=>"Table","field"=>"table_name");
+		$this->col[] = array("label"=>"Table","field"=>"table_name");		
+		$this->col[] = array("label"=>"Path","field"=>"path"); 		
 		$this->col[] = array("label"=>"Controller","field"=>"controller");
 		$this->col[] = array("label"=>"Group","field"=>"id_cms_moduls_group","join"=>"cms_moduls_group,nama_group");		
 		$this->col[] = array("label"=>"Active","field"=>"is_active","callback_php"=>"(%field%)?'<span class=\"label label-success\">Active</span>':'<span class=\"label label-default\">Not Active</span>'");		
 
 		$this->form = array();		
-		$this->form[] = array("label"=>"Name","name"=>"name");
+		$this->form[] = array("label"=>"Name","name"=>"name","placeholder"=>"Enter a module name",);
 
-		$exception_table = array('cms_dashboard','cms_logs','cms_moduls','cms_moduls_group','cms_privileges','cms_privileges_roles','cms_users','cms_apicustom','cms_settings','cms_companies','cms_filemanager');
 		$tables = DB::select('SHOW TABLES');
 		$tables_list = array();		
 		foreach($tables as $tab) {
@@ -43,29 +42,22 @@ class ModulsController extends Controller {
 				if(is_exists_controller($value)) {
 					$label = $label." [Already]";
 				}
+				
+				if(substr($value, 0,4)=='cms_') continue;
+
 				$tables_list[] = $value."|".$label;
+			}
+		}
+		foreach($tables as $tab) {
+			foreach ($tab as $key => $value) {	
+				$label = "[Default] ".$value;	
+				if(substr($value, 0,4)=='cms_') $tables_list[] = $value."|".$label;				
 			}
 		}
 
 
 
-		$this->form[] = array("label"=>"Table Name","name"=>"table_name","type"=>"select","dataenum"=>$tables_list);				
-
-		$this->form[] = array("label"=>"Route","name"=>"path","value"=>"admin/","jquery"=>"
-			$('#table_name').change(function() {
-				var v = $(this).val();
-				$('#path').val('admin/'+v);
-			})
-			");
-
-		$this->form[] = array("label"=>"Controller","name"=>"controller","type"=>"text","placeholder"=>"Auto Generated");
-		
-		$this->form[] = array("label"=>"SQL Where Query","name"=>"sql_where","type"=>"text","placeholder"=>"Example : columnname = value AND columnname2 = value2","help"=>"You can use alias [admin_id],[admin_id_companies]");
-
-		$this->form[] = array("label"=>"SQL Order By","name"=>"sql_orderby","type"=>"text","placeholder"=>"Enter query here","help"=>"Example : column_name ASC, column2_name DESC");
-		$this->form[] = ['label'=>"Limit Data","name"=>"limit_data","type"=>"text","placeholder"=>"Example : 10"];
-
-		$this->form[] = array("label"=>"Delete Data Mode","name"=>"is_softdelete","type"=>"radio","dataenum"=>['1|Soft Delete','0|Permanent Delete'],"help"=>"Soft Delete Note : Please make sure you have column 'deleted_at' with data type TIMESTAMP","value"=>0);
+		$this->form[] = array("label"=>"Table Name","name"=>"table_name","type"=>"select","dataenum"=>$tables_list);	
 
 		$this->form[] = array("label"=>"Icon","name"=>"icon","type"=>"radio","dataenum"=>array(
 				"fa fa-cog|<i class='fa fa-cog'></i>",
@@ -91,8 +83,20 @@ class ModulsController extends Controller {
 				"fa fa-inbox|<i class='fa fa-inbox'></i>",								
 				"fa fa-info-circle|<i class='fa fa-info-circle'></i>",
 				"fa fa-cloud-download|<i class='fa fa-cloud-download'></i>",
-			),"value"=>"fa fa-bars");
-		
+			),"value"=>"fa fa-bars");			
+
+
+		$this->form[] = array("label"=>"Route","name"=>"path","value"=>"admin/","jquery"=>"
+			$('#table_name').change(function() {
+				var v = $(this).val();
+				$('#path').val('admin/'+v);
+			})
+			");
+		$this->form[] = array("label"=>"Controller","name"=>"controller","type"=>"text","placeholder"=>"Auto Generated");
+		$this->form[] = array("label"=>"SQL Where Query","name"=>"sql_where","type"=>"text","placeholder"=>"Example : columnname = value AND columnname2 = value2","help"=>"You can use alias [admin_id],[admin_id_companies]");
+		$this->form[] = array("label"=>"SQL Order By","name"=>"sql_orderby","type"=>"text","placeholder"=>"Enter query here","help"=>"Example : column_name ASC, column2_name DESC");
+		$this->form[] = ['label'=>"Limit Data","name"=>"limit_data","type"=>"text","placeholder"=>"Example : 10"];
+		$this->form[] = array("label"=>"Delete Data Mode","name"=>"is_softdelete","type"=>"radio","dataenum"=>['1|Soft Delete','0|Permanent Delete'],"help"=>"Soft Delete Note : Please make sure you have column 'deleted_at' with data type TIMESTAMP","value"=>0);				
 		$this->form[] = array("label"=>"Group","name"=>"id_cms_moduls_group","required"=>"required","type"=>"select","datatable"=>"cms_moduls_group,nama_group");		
 		$this->form[] = array("label"=>"Active ?","name"=>"is_active","type"=>"select","dataenum"=>array("1|Active","0|Not Active"),"value"=>1);
 		
