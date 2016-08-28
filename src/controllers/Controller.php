deb4afb70b24bede54fbbbe11a731a119abd75ff
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-08-28 22:03:23 +0700

    bug fixed compatibility Controller

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -2,13 +2,13 @@
 
 namespace crocodicstudio\crudbooster\controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
+// use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+// use Illuminate\Foundation\Validation\ValidatesRequests;
+// use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+// use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    // use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 }
