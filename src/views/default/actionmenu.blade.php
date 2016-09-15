ecb12b5fbeb1afe12ec3d7f0b0e07a8ac4c1ff1a
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-09-15 13:40:43 +0700

    bug fix typo onMouseOver

--- src/views/default/actionmenu.blade.php
+++ src/views/default/actionmenu.blade.php
@@ -33,7 +33,7 @@
 	@foreach($index_button as $ib)
 		<a href='{{$ib["url"]}}' id='{{str_slug($ib["label"])}}' class='btn btn-app' 
 		@if($ib['onClick']) onClick='return {{$ib["onClick"]}}' @endif
-		@if($ib['onMouseOever']) onMouseOever='return {{$ib["onMouseOever"]}}' @endif
+		@if($ib['onMouseOver']) onMouseOver='return {{$ib["onMouseOver"]}}' @endif
 		@if($ib['onMoueseOut']) onMoueseOut='return {{$ib["onMoueseOut"]}}' @endif
 		@if($ib['onKeyDown']) onKeyDown='return {{$ib["onKeyDown"]}}' @endif
 		@if($ib['onLoad']) onLoad='return {{$ib["onLoad"]}}' @endif
