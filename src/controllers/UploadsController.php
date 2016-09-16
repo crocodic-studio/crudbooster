72ad9d96a59479c8b486f19ff6da858cb150b317
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-09-16 23:00:25 +0700

    improve upload controller support resize

--- src/controllers/UploadsController.php
+++ src/controllers/UploadsController.php
@@ -4,6 +4,9 @@ use crocodicstudio\crudbooster\controllers\Controller;
 
 use Storage;
 use Response;
+use Image;
+use File;
+use Request;
 
 class UploadsController extends Controller {
 
@@ -12,6 +15,26 @@ class UploadsController extends Controller {
 
 	    if(!Storage::exists($folder.DIRECTORY_SEPARATOR.$filename)) abort(404);
 
-	    return response()->file($path);
+	    $w = Request::get('w');
+	    $h = Request::get('h');
+	    $h = ($h)?:$w;
+
+	    $extension = File::extension($path);
+	    $images_ext = array('jpg','jpeg','png','gif','bmp');
+
+	    if(in_array($extension, $images_ext)) {	  
+	    	header('Content-Type: image/'.$extension);  
+	    	$img = Image::make($path);
+	    	if($w) {
+	    		if(!$h) {
+	    			$img->fit($w);
+	    		}else{
+	    			$img->fit($w,$h);
+	    		}	    		
+	    	}
+	    	return $img->response();
+	    }else{
+	    	return response()->file($path);
+	    }	    	    
 	}
 }
