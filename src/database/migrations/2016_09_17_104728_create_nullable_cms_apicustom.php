e231dd06926b33f0e373152d071c350ae725a13b
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-09-17 19:43:36 +0700

    add nullable for cms api custom

---
+++ src/database/migrations/2016_09_17_104728_create_nullable_cms_apicustom.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNullableCmsApicustom extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cms_apicustom', function (Blueprint $table) {
+            //
+            $table->dateTime('created_at')->nullable()->change();
+            $table->dateTime('updated_at')->nullable()->change();
+            $table->string('permalink')->nullable()->change();
+            $table->string('tabel')->nullable()->change();
+            $table->string('aksi')->nullable()->change();
+            $table->string('kolom')->nullable()->change();
+            $table->string('orderby')->nullable()->change();
+            $table->string('sub_query_1')->nullable()->change();
+            $table->string('sql_where')->nullable()->change();
+            $table->string('nama')->nullable()->change();
+            $table->string('keterangan')->nullable()->change();
+            $table->string('parameter')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cms_settings', function (Blueprint $table) {
+            //
+        });
+    }
+}
