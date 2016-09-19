f3d366ebbe1c680672e2c70934cc02f00e29103c
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-09-19 15:54:51 +0700

    improve Time and DatePicker when disabled

--- src/views/default/form_body.blade.php
+++ src/views/default/form_body.blade.php
@@ -528,8 +528,10 @@
 						@if(@$type=='datetime' || @$type=='datetimepicker')
 						<div class='form-group form-datepicker {{$header_group_class}} {{ ($errors->first($name))?"has-error":"" }}' id='form-group-{{$name}}' style="{{@$form['style']}}">
 							<label>{{$form['label']}} {!!($required)?"<span class='text-danger' title='This field is required'>*</span>":"" !!}</label>
-							<div class="input-group">  								
+							<div class="input-group">  				
+								@if(!$disabled)				
 								<span class="input-group-addon"><i class='fa fa-calendar'></i></span>
+								@endif
 								<input type='text' title="{{$form['label']}}" readonly {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} class='form-control notfocus datetimepicker' name="{{$name}}" id="{{$name}}" value='{{$value}}'/>					
 							</div>
 							<div class="text-danger">{!! $errors->first($name)?"<i class='fa fa-info-circle'></i> ".$errors->first($name):"" !!}</div>
@@ -542,8 +544,10 @@
 							<div class='form-group {{$header_group_class}} {{ ($errors->first($name))?"has-error":"" }}' id='form-group-{{$name}}' style="{{@$form['style']}}">
 								<label>{{$form['label']}} {!!($required)?"<span class='text-danger' title='This field is required'>*</span>":"" !!}</label>
 								<div class="input-group">	  								
-									<input type='text' title="{{$form['label']}}" {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} class='form-control notfocus timepicker' name="{{$name}}" id="{{$name}}" value='{{$value}}'/>
+									@if(!$disabled)
 									<span class="input-group-addon"><i class='fa fa-clock-o'></i></span>
+									@endif
+									<input type='text' title="{{$form['label']}}" {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} class='form-control notfocus timepicker' name="{{$name}}" id="{{$name}}" value='{{$value}}'/>									
 								</div>
 								<div class="text-danger">{!! $errors->first($name)?"<i class='fa fa-info-circle'></i> ".$errors->first($name):"" !!}</div>
 								<p class='help-block'>{{ @$form['help'] }}</p>
