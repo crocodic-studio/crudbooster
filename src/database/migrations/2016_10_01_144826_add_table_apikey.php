2d68ae5497c0aea670befcdfea7bb4f2fe0d19f2
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-10-05 01:07:16 +0700

    add table apikey

---
+++ src/database/migrations/2016_10_01_144826_add_table_apikey.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTableApikey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cms_apikey', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('screetkey')->nullable();
+            $table->integer('hit')->nullable();
+            $table->string('status',25)->default('active');            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cms_apikey');
+    }
+}
