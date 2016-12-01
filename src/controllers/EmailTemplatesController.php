8b5fe2752a51daf537b1c866ff21edbc4f32e64e
Ferry Ariawan <ferryyangpuny@gmail.com>
2016-12-01 17:08:04 +0700

    bug fix long content email

--- src/controllers/EmailTemplatesController.php
+++ src/controllers/EmailTemplatesController.php
@@ -44,7 +44,7 @@
 			$this->form[] = array("label"=>"Template Name","name"=>"name","type"=>"text","required"=>TRUE,"validation"=>"required|min:3|max:255|alpha_spaces","placeholder"=>"You can only enter the letter only");
 			$this->form[] = array("label"=>"Slug","type"=>"text","name"=>"slug","required"=>true,'validation'=>'required|unique:cms_email_templates,slug');
 			$this->form[] = array("label"=>"Subject","name"=>"subject","type"=>"text","required"=>TRUE,"validation"=>"required|min:3|max:255");
-			$this->form[] = array("label"=>"Content","name"=>"content","type"=>"wysiwyg","required"=>TRUE,"validation"=>"required|min:3|max:255");
+			$this->form[] = array("label"=>"Content","name"=>"content","type"=>"wysiwyg","required"=>TRUE,"validation"=>"required");
 			$this->form[] = array("label"=>"Description","name"=>"description","type"=>"text","required"=>TRUE,"validation"=>"required|min:3|max:255");
 			
 			$this->form[] = array("label"=>"From Name","name"=>"from_name","type"=>"text","required"=>false,"width"=>"col-sm-6",'placeholder'=>'Optional');
