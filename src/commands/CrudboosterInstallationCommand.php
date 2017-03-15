d6937dd2aa3a85f149e0ec6a54e95937dd093e9c
Ferry Ariawan <ferryyangpuny@gmail.com>
2017-03-16 01:47:09 +0700

    remove database configuration from artisan it cause bug

--- src/commands/CrudboosterInstallationCommand.php
+++ src/commands/CrudboosterInstallationCommand.php
@@ -36,95 +36,26 @@ class CrudboosterInstallationCommand extends Command {
 		
 		$this->info($this->description);
 		$this->info('---');
-		$this->info('Thank you for choose the CRUDBooster');
+		$this->info('Thank you for choosing the CRUDBooster');
 		$this->info('---');
 
-		$this->info('Publishing files...');
-		$this->callSilent('vendor:publish');	
-		$this->callSilent('vendor:publish',['--tag'=>'cb_migration','--force'=>'default']);
-		$this->callSilent('vendor:publish',['--tag'=>'cb_lfm','--force'=>'default']);	
-		$this->callSilent('vendor:publish',['--tag'=>'cb_localization','--force'=>'default']);	
+		if($this->confirm('Do you have setting the database configuration at .env ?')) {
 
-		$this->call('optimize');
-
-		$this->info('Setting The Database Configuration : ');
-		$default     = 'mysql';
-		$db_driver   = $this->choice('DB DRIVER (DEFAULT MYSQL) ? enter to skip', ['mysql', 'pgsql','sqlsrv'], $default);
-		$db_host     = $this->ask('DATABASE HOST (DEFAULT 127.0.0.1) ? enter to skip',false);
-		$db_host     = ($db_host === FALSE)?'127.0.0.1':$db_host;
-		$db_port     = $this->ask("DATABASE PORT (DEFAULT 3306) ? enter to skip",false);
-		$db_port     = ($db_port === FALSE)?'3306':$db_port;
-		$db_name     = $this->ask('DATABASE NAME ?');
-		$db_username = $this->ask('USERNAME ?');
-		$db_password = $this->ask('PASSWORD ?',false);
-		$db_password = ($db_password === FALSE)?'':$db_password;
-
-		copy(base_path('.env.example'),base_path('.env'));				
-
-
-		file_put_contents(App::environmentFilePath(), str_replace(	        
-	        'DB_CONNECTION=mysql',
-	        'DB_CONNECTION='.$db_driver,
-	        file_get_contents(App::environmentFilePath())
-	    ));
-
-	    file_put_contents(App::environmentFilePath(), str_replace(	        
-	        'DB_HOST=127.0.0.1',
-	        'DB_HOST='.$db_host,
-	        file_get_contents(App::environmentFilePath())
-	    ));
-
-	    file_put_contents(App::environmentFilePath(), str_replace(	        
-	        'DB_PORT=3306',
-	        'DB_PORT='.$db_port,
-	        file_get_contents(App::environmentFilePath())
-	    ));
-
-	    file_put_contents(App::environmentFilePath(), str_replace(	        
-	        'DB_DATABASE=homestead',
-	        'DB_DATABASE='.$db_name,
-	        file_get_contents(App::environmentFilePath())
-	    ));
-
-	    file_put_contents(App::environmentFilePath(), str_replace(	        
-	        'DB_USERNAME=homestead',
-	        'DB_USERNAME='.$db_username,
-	        file_get_contents(App::environmentFilePath())
-	    ));
-	    
-	    file_put_contents(App::environmentFilePath(), str_replace(	        
-	        'DB_PASSWORD=secret',
-	        'DB_PASSWORD='.$db_password,
-	        file_get_contents(App::environmentFilePath())
-	    ));
-
-	    $key = 'base64:'.base64_encode(str_random(32));
-    	file_put_contents(App::environmentFilePath(), str_replace(	        
-	        'APP_KEY=',
-	        'APP_KEY='.$key,
-	        file_get_contents(App::environmentFilePath())
-	    ));
-	   
-
-	    \Config::set('database.default',$db_driver);
-	    \Config::set('database.connections.'.$db_driver.'.driver',$db_driver);
-	    \Config::set('database.connections.'.$db_driver.'.host',$db_host);
-	    \Config::set('database.connections.'.$db_driver.'.port',$db_port);
-	    \Config::set('database.connections.'.$db_driver.'.database',$db_name);
-	    \Config::set('database.connections.'.$db_driver.'.username',$db_username);
-	    \Config::set('database.connections.'.$db_driver.'.password',$db_password);	    
-	    DB::reconnect($db_driver);
-
-		
-		$this->info('Migrating database...');		
-		
-		$this->callSilent('migrate',['--force'=>true]);		
-		$this->callSilent('db:seed');
-
-		$this->callSilent('config:clear');		
-		$this->call('optimize');
-		
-		$this->info('Install CRUDBooster Done !');
+			$this->info('Publishing files...');
+			$this->callSilent('vendor:publish');	
+			$this->callSilent('vendor:publish',['--tag'=>'cb_migration','--force'=>true]);
+			$this->callSilent('vendor:publish',['--tag'=>'cb_lfm','--force'=>true]);	
+			$this->callSilent('vendor:publish',['--tag'=>'cb_localization','--force'=>true]);		
+			
+			$this->info('Migrating database...');				
+			$this->call('migrate',['--seed'=>true]);				
+			$this->call('config:clear');		
+			$this->call('optimize');
+			
+			$this->info('Install CRUDBooster Done !');
+		}else{
+			$this->info('Please setting the database configuration for first !');
+		}
 	}
 
 }
