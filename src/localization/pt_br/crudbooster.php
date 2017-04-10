683f004c50874698992d320daccbd2ae233fd5b6
salihkiraz <salihk06@gmail.com>
2017-04-10 17:01:25 +0300

    pirivilages langauage update

--- src/localization/pt_br/crudbooster.php
+++ src/localization/pt_br/crudbooster.php
@@ -202,5 +202,22 @@ return [
 
     //filemanager
     'chose_an_image' =>'Escolha uma imagem ',
-    'chose_an_file' =>'Escolha um arquivo '
+    'chose_an_file' =>'Escolha um arquivo ',
+
+        // privilege
+    'privileges_name'               =>'Privilege Name',
+    'privileges_super_admin'        =>'Super Admin',
+    'privileges_standart_user'      =>'Standart user',
+    'privileges_ID'                 =>'ID',
+    'set_as_superadmin'             =>'Set as Superadmin',
+    'chose_theme_color_select'      =>'** Choose Backend Theme Color',
+    'chose_theme_color'             =>' Theme Color',
+    'privileges_configuration'      =>'Privileges Configuration',
+    'privileges_module_list_no'     =>'No.',
+    'privileges_module_list_mod_names'=>'Module\'s Name',
+    'privileges_module_list_view'   =>'View',
+    'privileges_module_list_create' =>'Create',
+    'privileges_module_list_read'   =>'Read',
+    'privileges_module_list_update' =>'Update',
+    'privileges_module_list_delete' =>'Delete',
 ];
