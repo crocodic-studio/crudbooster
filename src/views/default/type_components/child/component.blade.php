672f75864ff2219199fa38ed238bbf35f8fce15c
catfishseo <catfishseonet@gmail.com>
2017-04-13 13:01:00 -0500

    Support Hidden Input in Child Type
    This is a really small change, however I could not make hidden input works in child type without this code change. I will really appreciate if you can look at it and merge it to you repository.

--- src/views/default/type_components/child/component.blade.php
+++ src/views/default/type_components/child/component.blade.php
@@ -25,7 +25,9 @@
 							@foreach($form['columns'] as $col)	
 							<?php $name_column = $name.$col['name'];?>
 							<div class='form-group'>
+								@if($col['type']!='hidden')
 								<label class="control-label col-sm-2">{{$col['label']}}</label>
+								@endif
 								<div class="col-sm-10">
 									@if($col['type']=='text')
 									<input id='{{$name_column}}' type='text' {{ ($col['max'])?"maxlength='$col[max]'":"" }} name='{{$col["name"]}}' class='form-control {{$col['required']?"required":""}}' 										
@@ -255,6 +257,8 @@
 											}											
 										?>										
 									</select>
+									@elseif($col['type']=='hidden')
+										<input type="{{$col['type']}}" id="{{$name.$col["name"]}}" name="{{$name.$col["name"]}}" value="{{$col["value"]}}">
 									@endif
 
 									@if($col['help']) 
