1304da3c2d69eb72232bf0540aebe8e937055370
Scorpion <davo_bidzian@mail.ru>
2017-05-02 02:43:45 +0300

    Update forgot.blade.php
    fix jQuery version

--- src/views/forgot.blade.php
+++ src/views/forgot.blade.php
@@ -75,7 +75,7 @@
     </div><!-- /.login-box -->
 
     <!-- jQuery 2.1.3 -->
-    <script src="{{asset('vendor/crudbooster/assets/adminlte/plugins/jQuery/jQuery-2.1.3.min.js')}}"></script>
+    <script src="{{asset('vendor/crudbooster/assets/adminlte/plugins/jQuery/jQuery-2.1.4.min.js')}}"></script>
     <!-- Bootstrap 3.3.2 JS -->
     <script src="{{asset('vendor/crudbooster/assets/adminlte/bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
 
