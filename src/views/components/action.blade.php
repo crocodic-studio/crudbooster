916b01aad4f7c34b09455a703533d90d533bbb72
Tomás Aguirre <tomas@atiko7.com>
2017-05-17 01:56:10 -0300

    adds localization for confirm dialogue and fixes buttons text #534

--- src/views/components/action.blade.php
+++ src/views/components/action.blade.php
@@ -13,7 +13,8 @@
         $a['confirmation_type'] = !empty($a['confirmation_type']) ? $a['confirmation_type'] : 'warning';
         $a['confirmation_showCancelButton'] = empty($a['confirmation_showCancelButton']) ? 'true' : 'false';
         $a['confirmation_confirmButtonColor'] = !empty($a['confirmation_confirmButtonColor']) ? $a['confirmation_confirmButtonColor'] : '#DD6B55';
-        $a['confirmation_confirmButtonText'] = !empty($a['confirmation_confirmButtonText']) ? $a['confirmation_confirmButtonText'] : trans('crudbooster.confirmButtonText');;
+        $a['confirmation_confirmButtonText'] = !empty($a['confirmation_confirmButtonText']) ? $a['confirmation_confirmButtonText'] : trans('crudbooster.confirmation_yes');;
+        $a['confirmation_cancelButtonText'] = !empty($a['confirmation_cancelButtonText']) ? $a['confirmation_cancelButtonText'] : trans('crudbooster.confirmation_no');;
         $a['confirmation_closeOnConfirm'] = empty($a['confirmation_closeOnConfirm']) ? 'true' : 'false';
                 
         $confirm_box = '
@@ -24,6 +25,7 @@
             showCancelButton: '.$a['confirmation_showCancelButton'].',
             confirmButtonColor: "'.$a['confirmation_confirmButtonColor'].'",
             confirmButtonText: "'.$a['confirmation_confirmButtonText'].'",
+            cancelButtonText: "'.$a['confirmation_cancelButtonText'].'",
             closeOnConfirm: '.$a['confirmation_closeOnConfirm'].', }, 
             function(){  location.href="'.$a['url'].'"});        
 
@@ -42,7 +44,7 @@
     {
         $url = "javascript:;";
     }
-    
+
     if(isset($a['showIf'])) {
 
       $query = $a['showIf'];
