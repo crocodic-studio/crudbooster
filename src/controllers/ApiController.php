a1ea8a1c85c1e1753d0342e7e62d80a6f917858a
imanghafoori <imanghafoori1@gmail.com>
2017-08-30 09:42:02 +0430

    Avoid nested conditions

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -483,15 +483,15 @@ class ApiController extends Controller {
 		    	}
 		    }
 
-		    if($action_type == 'save_add') {
+		    if($action_type == 'save_add' && CRUDBooster::isColumnExists($table,'created_at')) {
 		    	if(CRUDBooster::isColumnExists($table,'created_at')) {
-		    		$row_assign['created_at'] = date('Y-m-d H:i:s');
+                $row_assign['created_at'] = date('Y-m-d H:i:s');
 		    	}
-		    } 
+		    }
 
-		    if($action_type == 'save_edit') {
+		    if($action_type == 'save_edit' && CRUDBooster::isColumnExists($table,'updated_at')) {
 		    	if(CRUDBooster::isColumnExists($table,'updated_at')) {
-		    		$row_assign['updated_at'] = date('Y-m-d H:i:s');
+                $row_assign['updated_at'] = date('Y-m-d H:i:s');
 		    	}
 		    }
 
