2233fd67068a19dbc9f53ba586485fec1834104f
imanghafoori <imanghafoori1@gmail.com>
2017-08-30 09:01:55 +0430

    simple refactors, removing else{}

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -38,9 +38,9 @@ class AdminController extends CBController {
 		if(\Hash::check($password,$users->password)) {
 			Session::put('admin_lock',0);	
 			return redirect()->route('AdminControllerGetIndex'); 
-		}else{
-			echo "<script>alert('".trans('crudbooster.alert_password_wrong')."');history.go(-1);</script>";				
 		}
+        echo "<script>alert('".trans('crudbooster.alert_password_wrong')."');history.go(-1);</script>";
+
 	}	
 
 	public function getLogin()
@@ -72,36 +72,37 @@ class AdminController extends CBController {
 		$password 	= Request::input("password");
 		$users 		= DB::table('cms_users')->where("email",$email)->first(); 		
 
-		if(\Hash::check($password,$users->password)) {
-			$priv = DB::table("cms_privileges")->where("id",$users->id_cms_privileges)->first();
+		if(!\Hash::check($password,$users->password)) {
+            return redirect()->route('getLogin')->with('message', trans('crudbooster.alert_password_wrong'));
+        }
 
-			$roles = DB::table('cms_privileges_roles')
-			->where('id_cms_privileges',$users->id_cms_privileges)
-			->join('cms_moduls','cms_moduls.id','=','id_cms_moduls')
-			->select('cms_moduls.name','cms_moduls.path','is_visible','is_create','is_read','is_edit','is_delete')
-			->get();
-			
-			$photo = ($users->photo)?asset($users->photo):asset('vendor/crudbooster/avatar.jpg');
-			Session::put('admin_id',$users->id);			
-			Session::put('admin_is_superadmin',$priv->is_superadmin);
-			Session::put('admin_name',$users->name);	
-			Session::put('admin_photo',$photo);
-			Session::put('admin_privileges_roles',$roles);
-			Session::put("admin_privileges",$users->id_cms_privileges);
-			Session::put('admin_privileges_name',$priv->name);			
-			Session::put('admin_lock',0);
-			Session::put('theme_color',$priv->theme_color);
-			Session::put("appname",CRUDBooster::getSetting('appname'));		
+        $priv = DB::table("cms_privileges")->where("id",$users->id_cms_privileges)->first();
 
-			CRUDBooster::insertLog(trans("crudbooster.log_login",['email'=>$users->email,'ip'=>Request::server('REMOTE_ADDR')]));		
+        $roles = DB::table('cms_privileges_roles')
+        ->where('id_cms_privileges',$users->id_cms_privileges)
+        ->join('cms_moduls','cms_moduls.id','=','id_cms_moduls')
+        ->select('cms_moduls.name','cms_moduls.path','is_visible','is_create','is_read','is_edit','is_delete')
+        ->get();
 
-			$cb_hook_session = new \App\Http\Controllers\CBHook;
-			$cb_hook_session->afterLogin();
+        $photo = ($users->photo)?asset($users->photo):asset('vendor/crudbooster/avatar.jpg');
+        Session::put('admin_id',$users->id);
+        Session::put('admin_is_superadmin',$priv->is_superadmin);
+        Session::put('admin_name',$users->name);
+        Session::put('admin_photo',$photo);
+        Session::put('admin_privileges_roles',$roles);
+        Session::put("admin_privileges",$users->id_cms_privileges);
+        Session::put('admin_privileges_name',$priv->name);
+        Session::put('admin_lock',0);
+        Session::put('theme_color',$priv->theme_color);
+        Session::put("appname",CRUDBooster::getSetting('appname'));
 
-			return redirect(CRUDBooster::adminPath()); 
-		}else{
-			return redirect()->route('getLogin')->with('message', trans('crudbooster.alert_password_wrong'));			
-		}		
+        CRUDBooster::insertLog(trans("crudbooster.log_login",['email'=>$users->email,'ip'=>Request::server('REMOTE_ADDR')]));
+
+        $cb_hook_session = new \App\Http\Controllers\CBHook;
+        $cb_hook_session->afterLogin();
+
+        return redirect(CRUDBooster::adminPath());
+
 	}
 
 	public function getForgot() {	
