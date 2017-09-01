41ef395aa2eeebbb14fc8c499099d5ffae09b98c
imanghafoori <imanghafoori1@gmail.com>
2017-09-01 16:05:09 +0430

    else{} Removed

--- src/controllers/AdminModulesController.php
+++ src/controllers/AdminModulesController.php
@@ -243,25 +243,26 @@ class AdminModulesController extends CBController {
 			->get();
 			Session::put('admin_privileges_roles',$roles);
 			return redirect(Route("AdminModulesControllerGetStep2",["id"=>$id]));
-		}else{
-			$id = Request::get('id');
-			DB::table($this->table)->where('id',$id)->update(compact("name","table_name","icon","path"));	
 
-			$row = DB::table('cms_moduls')->where('id',$id)->first();
-			
-			if(file_exists(app_path('Http/Controllers/'.$row->controller.'.php'))) {
-				$response = file_get_contents(app_path('Http/Controllers/'.str_replace('.', '', $row->controller).'.php'));	
-			}else{
-				$response = file_get_contents(__DIR__.'/'.str_replace('.', '', $row->controller).'.php');	
-			}			
-
-			if(strpos($response, "# START COLUMNS") !== TRUE) {
-				// return redirect()->back()->with(['message'=>'Sorry, is not possible to edit the module with Module Generator Tool. Prefix and or Suffix tag is missing !','message_type'=>'warning']);	
-			}
-
-			
-			return redirect(Route("AdminModulesControllerGetStep2",["id"=>$id]));		
 		}
+
+        $id = Request::get('id');
+        DB::table($this->table)->where('id',$id)->update(compact("name","table_name","icon","path"));
+
+        $row = DB::table('cms_moduls')->where('id',$id)->first();
+
+        $response = file_get_contents(__DIR__.'/'.str_replace('.', '', $row->controller).'.php');
+        if(file_exists(app_path('Http/Controllers/'.$row->controller.'.php'))) {
+            $response = file_get_contents(app_path('Http/Controllers/'.str_replace('.', '', $row->controller).'.php'));
+        }
+
+        if(strpos($response, "# START COLUMNS") !== TRUE) {
+            // return redirect()->back()->with(['message'=>'Sorry, is not possible to edit the module with Module Generator Tool. Prefix and or Suffix tag is missing !','message_type'=>'warning']);
+        }
+
+
+        return redirect(Route("AdminModulesControllerGetStep2",["id"=>$id]));
+
 	}
 
 	public function postStep3() {
