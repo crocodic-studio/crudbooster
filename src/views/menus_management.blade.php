9fe469474f967dc975d94c0d3725a2bae19ce46f
imanghafoori <imanghafoori1@gmail.com>
2017-09-02 18:31:50 +0430

    refactoring to meaningful partials

--- src/views/menus_management.blade.php
+++ src/views/menus_management.blade.php
@@ -2,58 +2,7 @@
 @section('content')  
         
         @push('head')
-         <style type="text/css">
-           body.dragging, body.dragging * {
-              cursor: move !important;
-            }
-
-            .dragged {
-              position: absolute;
-              opacity: 0.7;
-              z-index: 2000;              
-            }
-
-            .draggable-menu {
-              padding: 0 0 0 0;
-              margin:0 0 0 0;
-            }
-
-            .draggable-menu li ul {
-              margin-top:6px;
-            }
-  
-            .draggable-menu li div {
-              padding:5px;
-              border:1px solid #cccccc;
-              background:#eeeeee;
-              cursor: move;
-            }
-
-            .draggable-menu li .is-dashboard {
-              background: #fff6e0;              
-            }  
-
-            .draggable-menu li .icon-is-dashboard {
-              color: #ffb600;
-            }
-
-            .draggable-menu li {
-              list-style-type:none;                            
-              margin-bottom:4px;
-              min-height: 35px;
-            }
-
-            .draggable-menu li.placeholder {
-              position: relative;
-              border:1px dashed #b7042c;
-              background:#ffffff;
-              /** More li styles **/
-            }
-            .draggable-menu li.placeholder:before {
-              position: absolute;
-              /** Define arrowhead **/
-            }
-         </style>
+            @include('crudbooster::_menus_management.css')
          @endpush
 
          @push('bottom')
@@ -77,53 +26,7 @@
         @endpush
          @push('bottom')
          <script src='{{asset("vendor/crudbooster/assets/jquery-sortable-min.js")}}'></script>
-         <script type="text/javascript">
-           $(function  () {
-            var id_cms_privileges = '{{$id_cms_privileges}}';
-            var sortactive = $(".draggable-menu").sortable({
-              group: '.draggable-menu',  
-              delay:200,               
-              isValidTarget: function ($item, container) {
-                  var depth = 1, // Start with a depth of one (the element itself)
-                      maxDepth = 2,
-                      children = $item.find('ul').first().find('li');
-
-                  // Add the amount of parents to the depth
-                  depth += container.el.parents('ul').length;
-
-                  // Increment the depth for each time a child
-                  while (children.length) {
-                      depth++;
-                      children = children.find('ul').first().find('li');
-                  }
-
-                  return depth <= maxDepth;
-              },                       
-              onDrop: function ($item, container, _super) {                                                    
-
-                if($item.parents('ul').hasClass('draggable-menu-active')) {
-                  var isActive = 1;
-                  var data = $('.draggable-menu-active').sortable("serialize").get();
-                  var jsonString = JSON.stringify(data, null, ' '); 
-                }else{
-                  var isActive = 0;
-                  var data = $('.draggable-menu-inactive').sortable("serialize").get();
-                  var jsonString = JSON.stringify(data, null, ' ');
-                  $('#inactive_text').remove();
-                }
-
-                $.post("{{route('AdminMenusControllerPostSaveMenu')}}",{menus:jsonString,isActive:isActive},function(resp) {
-                  $('#menu-saved-info').fadeIn('fast').delay(1000).fadeOut('fast');
-                });
-           
-                _super($item, container);
-              }
-            });    
-
-
-
-          });
-         </script>
+        @include('crudbooster::_menus_management.js')
          @endpush     
 
          <div class='row'>
@@ -187,13 +90,13 @@
                       Add Menu
                     </div>
                     <div class="panel-body">
-                      <form class='form-horizontal' method='post' id="form" enctype="multipart/form-data" action='{{CRUDBooster::mainpath("add-save")}}'>
-                      <input type="hidden" name="_token" value="{{ csrf_token() }}">  
-                      <input type='hidden' name='return_url' value='{{Request::fullUrl()}}'/>
+
+
                       @include("crudbooster::default.form_body")
-                      <p align="right"><input type='submit' class='btn btn-primary' value='Add Menu'/></p>
-                      </form>
-                    </div>             
+
+
+
+                    </div>
                   </div>
               </div>
          </div>
