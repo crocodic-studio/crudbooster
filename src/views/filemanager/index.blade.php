8ba216edd4cec104f54e21e1048b4d090e82d850
imanghafoori <imanghafoori1@gmail.com>
2017-09-02 17:02:23 +0430

    Putting modals in meaningful partials

--- src/views/filemanager/index.blade.php
+++ src/views/filemanager/index.blade.php
@@ -84,68 +84,22 @@
 	    </div>
 	</div>
 
-	@push('bottom') 
+	@push('bottom')
 		<script type="text/javascript">
 			function showModalCreateDirectory() {
 				$('#modal-create-directory').modal('show');
 			}
 		</script>
-		<div id='modal-create-directory' class="modal fade" tabindex="-1" role="dialog">
-		  <div class="modal-dialog" role="document">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		        <h4 class="modal-title">Create a directory</h4>
-		      </div>
-		      <form method="post" action="{{route('AdminFileManagerControllerPostCreateDirectory')}}">
-		      <input type="hidden" name='_token' value="{{csrf_token()}}">
-		      <input type="hidden" name="path" value="{{g('path')}}">
-		      <div class="modal-body">
-		        <div class="form-group">
-		        	<label>Directory Name</label>
-		        	<input type="text" name="name" class="form-control" required >
-		        </div>
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-		        <button type="submit" class="btn btn-primary">Create</button>
-		      </div>
-		      </form>
-		    </div><!-- /.modal-content -->
-		  </div><!-- /.modal-dialog -->
-		</div><!-- /.modal -->
+		@include('crudbooster::filemanager.partials.create_folder_modal')
+
+
 
 		<script type="text/javascript">
 			function showModalUpload() {
 				$('#modal-upload-file').modal('show');
 			}
 		</script>
-		<div id='modal-upload-file' class="modal fade" tabindex="-1" role="dialog">
-		  <div class="modal-dialog" role="document">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		        <h4 class="modal-title">Upload a file</h4>
-		      </div>
-		      <form enctype="multipart/form-data" method="post" action="{{route('AdminFileManagerControllerPostUpload')}}">
-		      <input type="hidden" name='_token' value="{{csrf_token()}}">
-		      <input type="hidden" name="path" value="{{g('path')}}">
-		      <div class="modal-body">
-		        <div class="form-group">
-		        	<label>Filename</label>
-		        	<input type="file" name="userfile" class="form-control" required >
-		        	<div class="help-block">
-		        		File type support only {{ config('crudbooster.UPLOAD_TYPES') }}
-		        	</div>
-		        </div>
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-		        <button type="submit" class="btn btn-primary">Upload</button>
-		      </div>
-		      </form>
-		    </div><!-- /.modal-content -->
-		  </div><!-- /.modal-dialog -->
-		</div><!-- /.modal -->
+		@include('crudbooster::filemanager.partials.file_upload_modal')
+
 	@endpush
 @endsection
