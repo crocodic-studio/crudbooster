82742dbbb1de26ab6f32b2033cfeb1ee0f5624b0
imanghafoori <imanghafoori1@gmail.com>
2017-09-04 21:03:15 +0430

    re-factored into meaningful partials

---
+++ src/views/module_generator/step4/footer.blade.php
@@ -0,0 +1,7 @@
+<div align="right">
+    <button type="button" onclick="location.href='{{CRUDBooster::mainpath('step3').'/'.$id}}'"
+            class="btn btn-default">&laquo; Back
+    </button>
+    <input type="submit" name="submit" class='btn btn-primary' value='Save Module'>
+</div>
+            
---
+++ src/views/module_generator/step4/text_field.blade.php
@@ -0,0 +1,35 @@
+<div class="col-sm-12">
+    <div class="form-group">
+        <label>Title Field Candidate</label>
+        <input type="text" name="title_field" value="{{$config['title_field']}}" class='form-control'>
+    </div>
+</div>
+
+
+<div class="col-sm-5">
+    <div class="form-group">
+        <label>Limit Data</label>
+        <input type="number" name="limit" value="{{$config['limit']}}" class='form-control'>
+    </div>
+</div>
+
+
+<div class="col-sm-7">
+    <div class="form-group">
+        <label>Order By</label>
+        <?php
+        if (is_array($config['orderby'])) {
+            $orderby = [];
+            foreach ($config['orderby'] as $k => $v) {
+                $orderby[] = $k . ',' . $v;
+            }
+            $orderby = implode(";", $orderby);
+        } else {
+            $orderby = $config['orderby'];
+        }
+        ?>
+        <input type="text" name="orderby" value="{{ $orderby }}" class='form-control'>
+        <div class="help-block">E.g : column_name,desc</div>
+    </div>
+</div>
+                
