f835d29f3e99a843b5a9a41e68d6473b913404a2
imanghafoori <imanghafoori1@gmail.com>
2017-09-05 02:36:51 +0430

    clean up

--- src/views/_login/form.blade.php
+++ src/views/_login/form.blade.php
@@ -11,14 +11,16 @@
     </div>
     <div style="margin-bottom:10px" class='row'>
         <div class='col-xs-12'>
-            <button type="submit" class="btn btn-primary btn-block btn-flat"><i
-                        class='fa fa-lock'></i> {{cbTrans("button_sign_in")}}</button>
+            <button type="submit" class="btn btn-primary btn-block btn-flat">
+                {!! CB::icon('lock') !!} {{cbTrans("button_sign_in")}}</button>
         </div>
     </div>
 
     <div class='row'>
         <div class='col-xs-12' align="center">
             <p style="padding:10px 0px 10px 0px">{{cbTrans("text_forgot_password")}}
-                <a href='{{route("getForgot")}}'>{{cbTrans("click_here")}}</a></p></div>
+                <a href='{{route("getForgot")}}'>{{cbTrans("click_here")}}</a>
+            </p>
+        </div>
     </div>
 </form>
