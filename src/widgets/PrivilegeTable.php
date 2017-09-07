c69e922c74e1064d80d4ee7d9a13b3605835dfe7
imanghafoori <imanghafoori1@gmail.com>
2017-09-08 00:16:37 +0430

    Bug fix

--- src/widgets/PrivilegeTable.php
+++ src/widgets/PrivilegeTable.php
@@ -10,7 +10,7 @@ class PrivilegeTable
         $modules = \DB::table("cms_moduls")->where('is_protected',0)->select("cms_moduls.*")->orderby("name","asc")->get(['id','name']);
 
         foreach ($modules as $module){
-            $module->privilege = DB::table('cms_privileges_roles')
+            $module->privilege = \DB::table('cms_privileges_roles')
                 ->where('id_cms_moduls', $module->id)
                 ->where('id_cms_privileges', $roleId)
                 ->first() ?: new \stdClass();
