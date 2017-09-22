a6798b77e46fa8995eada71d83a87874ae6b85cd
imanghafoori <imanghafoori1@gmail.com>
2017-09-23 00:45:03 +0330

    fix

--- src/controllers/Forms/NotificationForm.php
+++ src/controllers/Forms/NotificationForm.php
@@ -2,36 +2,15 @@
 
 namespace crocodicstudio\crudbooster\controllers\Forms;
 
-class UsersForm
+class NotificationForm
 {
     public static function makeForm()
     {
         $form = [];
-        $form[] = ["label" => "Name", "name" => "name", 'required' => true, 'validation' => 'required|alpha_spaces|min:3'];
-        $form[] = [
-            "label" => "Email",
-            "name" => "email",
-            'required' => true,
-            'type' => 'email',
-            'validation' => 'required|email|unique:cms_users,email,'.\CRUDBooster::getCurrentId(),
-        ];
-        $form[] = [
-            "label" => "Photo",
-            "name" => "photo",
-            "type" => "upload",
-            "help" => "Recommended resolution is 200x200px",
-            'required' => true,
-            'validation' => 'required|image|max:1000',
-        ];
-        $form[] = [
-            "label" => "Privilege",
-            "name" => "id_cms_privileges",
-            "type" => "select_datatable",
-            "options" => ["table" => "cms_privileges", "field_value" => "id", "field_label" => "name"],
-            'required' => true,
-        ];
-        $form[] = ["label" => "Password", "name" => "password", "type" => "password", "help" => "Please leave empty if not change"];
-
+        $form[] = ["label" => "Content", "name" => "content", "type" => "text"];
+        $form[] = ["label" => "Icon", "name" => "icon", "type" => "text"];
+        $form[] = ["label" => "Notification Command", "name" => "notification_command", "type" => "textarea"];
+        $form[] = ["label" => "Is Read", "name" => "is_read", "type" => "text"];
         return $form;
     }
 }
