74461b0faa8a24acd650f4eff914927dd5f1ede5
imanghafoori <imanghafoori1@gmail.com>
2017-09-22 23:24:16 +0330

    refactor into private method

--- src/controllers/AdminNotificationsController.php
+++ src/controllers/AdminNotificationsController.php
@@ -31,21 +31,9 @@ class AdminNotificationsController extends CBController
         $this->button_import = false;
         $this->global_privilege = true;
 
-        $read_notification_url = url(cbConfig('ADMIN_PATH')).'/notifications/read';
+        $this->makeColumns();
 
-        $this->col = [];
-        $this->col[] = ["label" => "Content", "name" => "content", "callback_php" => '"<a href=\"'.$read_notification_url.'/$row->id\">$row->content</a>"'];
-        $this->col[] = [
-            'label' => 'Read',
-            'name' => 'is_read',
-            'callback_php' => '($row->is_read)?"<span class=\"label label-default\">Already Read</span>":"<span class=\"label label-danger\">NEW</span>"',
-        ];
-
-        $this->form = [];
-        $this->form[] = ["label" => "Content", "name" => "content", "type" => "text"];
-        $this->form[] = ["label" => "Icon", "name" => "icon", "type" => "text"];
-        $this->form[] = ["label" => "Notification Command", "name" => "notification_command", "type" => "textarea"];
-        $this->form[] = ["label" => "Is Read", "name" => "is_read", "type" => "text"];
+        $this->makeForm();
     }
 
     public function hook_query_index(&$query)
@@ -69,5 +57,27 @@ class AdminNotificationsController extends CBController
         $row = DB::table('cms_notifications')->where('id', $id)->first();
 
         return redirect($row->url);
+    }
+
+    private function makeForm()
+    {
+        $this->form = [];
+        $this->form[] = ["label" => "Content", "name" => "content", "type" => "text"];
+        $this->form[] = ["label" => "Icon", "name" => "icon", "type" => "text"];
+        $this->form[] = ["label" => "Notification Command", "name" => "notification_command", "type" => "textarea"];
+        $this->form[] = ["label" => "Is Read", "name" => "is_read", "type" => "text"];
+    }
+
+    private function makeColumns()
+    {
+        $read_notification_url = url(cbConfig('ADMIN_PATH')).'/notifications/read';
+
+        $this->col = [];
+        $this->col[] = ["label" => "Content", "name" => "content", "callback_php" => '"<a href=\"'.$read_notification_url.'/$row->id\">$row->content</a>"'];
+        $this->col[] = [
+            'label' => 'Read',
+            'name' => 'is_read',
+            'callback_php' => '($row->is_read)?"<span class=\"label label-default\">Already Read</span>":"<span class=\"label label-danger\">NEW</span>"',
+        ];
     }
 }
