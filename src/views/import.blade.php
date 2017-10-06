64e175da387446daec53bb3a336b7e8e9c0fae67
imanghafoori <imanghafoori1@gmail.com>
2017-10-06 23:03:50 +0330

    extracting partials

--- src/views/import.blade.php
+++ src/views/import.blade.php
@@ -12,18 +12,8 @@
 
 
     @if(request('file') && request('import'))
-
         <ul class='nav nav-tabs'>
-            <li style="background:#eeeeee">
-                <a style="color:#111" onclick="if(confirm('Are you sure want to leave ?')) location.href='{{ CRUDBooster::mainpath("import-data") }}'"
-                                              href='javascript:;'>
-                    {!! CB::icon('download') !!} Upload a File &raquo;
-                </a>
-            </li>
-            <li style="background:#eeeeee"><a style="color:#111" href='#'>{!! CB::icon('cogs') !!} Adjustment &raquo;</a>
-            </li>
-            <li style="background:#ffffff" class='active'><a style="color:#111" href='#'>
-                    {!! CB::icon('cloud-download') !!} Importing &raquo;</a></li>
+            @include('crudbooster::_import.tabs')
         </ul>
 
         <div id='box_main' class="box box-primary">
