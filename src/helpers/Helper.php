717e1c8c63e78c7aa51986b1193d35c01c65825d
imanghafoori <imanghafoori1@gmail.com>
2017-10-08 18:31:35 +0330

    flatten code

--- src/helpers/Helper.php
+++ src/helpers/Helper.php
@@ -396,19 +396,21 @@ if (! function_exists('rrmdir')) {
     */
     function rrmdir($dir)
     {
-        if (is_dir($dir)) {
-            $objects = scandir($dir);
-            foreach ($objects as $object) {
-                if ($object != "." && $object != "..") {
-                    if (is_dir($dir."/".$object)) {
-                        rrmdir($dir."/".$object);
-                    } else {
-                        unlink($dir."/".$object);
-                    }
-                }
-            }
-            rmdir($dir);
+        if (!is_dir($dir)) {
+            return;
         }
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object == "." || $object == "..") {
+                continue;
+            }
+            if (is_dir($dir."/".$object)) {
+                rrmdir($dir."/".$object);
+            } else {
+                unlink($dir."/".$object);
+            }
+        }
+        rmdir($dir);
     }
 }
 
