2a4d460c331f6dab8b791aa3ba1fb9034de80f4a
imanghafoori <imanghafoori1@gmail.com>
2017-10-08 19:41:40 +0330

    flatten code

--- src/helpers/CRUDBooster.php
+++ src/helpers/CRUDBooster.php
@@ -909,67 +909,67 @@ class CRUDBooster
 
     public static function authAPI()
     {
-        if (self::getSetting('api_debug_mode') == 'false') {
-
-            $result = [];
-            $validator = Validator::make([
-
-                'X-Authorization-Token' => Request::header('X-Authorization-Token'),
-                'X-Authorization-Time' => Request::header('X-Authorization-Time'),
-                'useragent' => Request::header('User-Agent'),
-            ], [
-                    'X-Authorization-Token' => 'required',
-                    'X-Authorization-Time' => 'required',
-                    'useragent' => 'required',
-                ]);
-
-            if ($validator->fails()) {
-                $message = $validator->errors()->all();
-                $result['api_status'] = 0;
-                $result['api_message'] = implode(', ', $message);
-                $res = response()->json($result, 200);
-                $res->send();
-                exit;
-            }
-
-            $user_agent = Request::header('User-Agent');
-            $time = Request::header('X-Authorization-Time');
-
-            $keys = DB::table('cms_apikey')->where('status', 'active')->pluck('screetkey');
-            $server_token = [];
-            $server_token_screet = [];
-            foreach ($keys as $key) {
-                $server_token[] = md5($key.$time.$user_agent);
-                $server_token_screet[] = $key;
-            }
-
-            $sender_token = Request::header('X-Authorization-Token');
-
-            if (! Cache::has($sender_token) && ! in_array($sender_token, $server_token)) {
-                $result['api_status'] = false;
-                $result['api_message'] = "THE TOKEN IS NOT MATCH WITH SERVER TOKEN";
-                $result['sender_token'] = $sender_token;
-                $result['server_token'] = $server_token;
-                response()->json($result, 200)->send();
-                exit;
-            }
-
-            if (Cache::has($sender_token) && Cache::get($sender_token) != $user_agent) {
-                $result['api_status'] = false;
-                $result['api_message'] = "THE TOKEN IS ALREADY BUT NOT MATCH WITH YOUR DEVICE";
-                $result['sender_token'] = $sender_token;
-                $result['server_token'] = $server_token;
-                response()->json($result, 200)->send();
-                exit;
-            }
-
-            $id = array_search($sender_token, $server_token);
-            $server_screet = $server_token_screet[$id];
-            DB::table('cms_apikey')->where('screetkey', $server_screet)->increment('hit');
-
-            $expired_token = date('Y-m-d H:i:s', strtotime('+5 seconds'));
-            Cache::put($sender_token, $user_agent, $expired_token);
+        if (self::getSetting('api_debug_mode') !== 'false') {
+            return ;
         }
+
+        $result = [];
+        $validator = Validator::make([
+
+            'X-Authorization-Token' => Request::header('X-Authorization-Token'),
+            'X-Authorization-Time' => Request::header('X-Authorization-Time'),
+            'useragent' => Request::header('User-Agent'),
+        ], [
+                'X-Authorization-Token' => 'required',
+                'X-Authorization-Time' => 'required',
+                'useragent' => 'required',
+            ]);
+
+        if ($validator->fails()) {
+            $message = $validator->errors()->all();
+            $result['api_status'] = 0;
+            $result['api_message'] = implode(', ', $message);
+           response()->json($result, 200)->send();
+            exit;
+        }
+
+        $user_agent = Request::header('User-Agent');
+        $time = Request::header('X-Authorization-Time');
+
+        $keys = DB::table('cms_apikey')->where('status', 'active')->pluck('screetkey');
+        $server_token = [];
+        $server_token_screet = [];
+        foreach ($keys as $key) {
+            $server_token[] = md5($key.$time.$user_agent);
+            $server_token_screet[] = $key;
+        }
+
+        $sender_token = Request::header('X-Authorization-Token');
+
+        if (! Cache::has($sender_token) && ! in_array($sender_token, $server_token)) {
+            $result['api_status'] = false;
+            $result['api_message'] = "THE TOKEN IS NOT MATCH WITH SERVER TOKEN";
+            $result['sender_token'] = $sender_token;
+            $result['server_token'] = $server_token;
+            response()->json($result, 200)->send();
+            exit;
+        }
+
+        if (Cache::has($sender_token) && Cache::get($sender_token) != $user_agent) {
+            $result['api_status'] = false;
+            $result['api_message'] = "THE TOKEN IS ALREADY BUT NOT MATCH WITH YOUR DEVICE";
+            $result['sender_token'] = $sender_token;
+            $result['server_token'] = $server_token;
+            response()->json($result, 200)->send();
+            exit;
+        }
+
+        $id = array_search($sender_token, $server_token);
+        $server_screet = $server_token_screet[$id];
+        DB::table('cms_apikey')->where('screetkey', $server_screet)->increment('hit');
+
+        $expired_token = date('Y-m-d H:i:s', strtotime('+5 seconds'));
+        Cache::put($sender_token, $user_agent, $expired_token);
     }
 
     public static function sendNotification($config = [])
