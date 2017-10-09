e5ecbf5715e3ba43bcdced84d80d30191a167b83
imanghafoori <imanghafoori1@gmail.com>
2017-10-10 00:25:39 +0330

    extracting a partial

--- src/views/admin_template.blade.php
+++ src/views/admin_template.blade.php
@@ -173,24 +173,7 @@
         <!-- Main content -->
         <section id='content_section' class="content">
 
-            @if(@$alerts)
-                @foreach(@$alerts as $alert)
-                    <div class='callout callout-{{$alert[type]}}'>
-                        {!! $alert['message'] !!}
-                    </div>
-                @endforeach
-            @endif
-
-
-            @if (Session::get('message')!='')
-                <div class='alert alert-{{ Session::get("message_type") }}'>
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    <h4>
-                        <i class="icon fa fa-info"></i>
-                        {{ cbTrans("alert_".Session::get("message_type")) }}</h4>
-                    {!!Session::get('message')!!}
-                </div>
-            @endif
+            @include('crudbooster::_admin_template.alert')
 
 
 
