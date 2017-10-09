f2cfebbb275e4265de0929680abfab716122b415
imanghafoori <imanghafoori1@gmail.com>
2017-10-10 00:33:26 +0330

    extracting a partial

--- src/views/admin_template.blade.php
+++ src/views/admin_template.blade.php
@@ -1,84 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
     <title>{{ ($page_title)?CRUDBooster::getSetting('appname').': '.strip_tags($page_title):"Admin Area" }}</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <meta name='generator' content='CRUDBooster 5.4.0.1'/>
-    <meta name='robots' content='noindex,nofollow'/>
-    <link rel="shortcut icon"
-          href="{{ CRUDBooster::getSetting('favicon')?asset(CRUDBooster::getSetting('favicon')):cbAsset('logo_crudbooster.png') }}">
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-
-{!! cbStyleSheet('adminlte/bootstrap/css/bootstrap.min.css') !!}
-
-{!! cbStyleSheet('adminlte/font-awesome/css/font-awesome.min.css') !!}
-<!-- Ionicons -->
-    <link href="{{asset("vendor/crudbooster/ionic/css/ionicons.min.css")}}" rel="stylesheet" type="text/css"/>
-    <!-- Theme style -->
-{!! cbStyleSheet('adminlte/dist/css/AdminLTE.min.css') !!}
-{!! cbStyleSheet('adminlte/dist/css/skins/_all-skins.min.css') !!}
-
-<!-- support rtl-->
-    @if (App::getLocale() == 'ar')
-        <link rel="stylesheet" href="//cdn.rawgit.com/morteza/bootstrap-rtl/v3.3.4/dist/css/bootstrap-rtl.min.css">
-        <link href="{{ cbAsset("rtl.css")}}" rel="stylesheet" type="text/css"/>
-    @endif
-
-    <link rel='stylesheet' href='{{cbAsset("css/main.css").'?r='.time()}}'/>
-
-    <!-- load css -->
-    <style type="text/css">
-        @if($style_css)
-            {!! $style_css !!}
-        @endif
-    </style>
-    @if($load_css)
-        @foreach($load_css as $css)
-            <link href="{{$css}}" rel="stylesheet" type="text/css"/>
-        @endforeach
-    @endif
-
-    <style type="text/css">
-        .dropdown-menu-action {
-            left: -130%;
-        }
-
-        .btn-group-action .btn-action {
-            cursor: default
-        }
-
-        #box-header-module {
-            box-shadow: 10px 10px 10px #dddddd;
-        }
-
-        .sub-module-tab li {
-            background: #F9F9F9;
-            cursor: pointer;
-        }
-
-        .sub-module-tab li.active {
-            background: #ffffff;
-            box-shadow: 0px -5px 10px #cccccc
-        }
-
-        .nav-tabs > li.active > a, .nav-tabs > li.active > a:focus, .nav-tabs > li.active > a:hover {
-            border: none;
-        }
-
-        .nav-tabs > li > a {
-            border: none;
-        }
-
-        .breadcrumb {
-            margin: 0 0 0 0;
-            padding: 0 0 0 0;
-        }
-
-        .form-group > label:first-child {
-            display: block
-        }
-    </style>
+    @include('crudbooster::_admin_template.meta')
+    @include('crudbooster::_admin_template.css')
 
     @stack('head')
 </head>
@@ -150,11 +75,8 @@
 
             @include('crudbooster::_admin_template.alert')
 
-
-
-        <!-- Your Page Content Here -->
             @yield('content')
-        </section><!-- /.content -->
+        </section>
     </div>
 
 
