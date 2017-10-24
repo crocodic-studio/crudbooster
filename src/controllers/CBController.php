86d08f2197c1f7e691e67184f7fcda1dfedd46dd
imanghafoori <imanghafoori1@gmail.com>
2017-10-24 07:24:01 +0330

    extract import logic to a class

--- src/controllers/CBController.php
+++ src/controllers/CBController.php
@@ -204,7 +204,6 @@ class CBController extends Controller
 
     public function getExportData()
     {
-
         return redirect(CRUDBooster::mainpath());
     }
 
@@ -823,94 +822,13 @@ class CBController extends Controller
     public function postDoImportChunk(IndexImport $import)
     {
         $this->cbLoader();
-        $file_md5 = md5(request('file'));
+        $fileMD5 = md5(request('file'));
 
         if (request('file') && request('resume') == 1) {
-            return $import->handleImportProgress($file_md5);
+            return $import->handleImportProgress($fileMD5);
         }
 
-        $select_column = array_filter(Session::get('select_column'));
-        $table_columns = DB::getSchemaBuilder()->getColumnListing($this->table);
-
-        $file = base64_decode(request('file'));
-        $file = 'storage'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.$file;
-        $rows = Excel::load($file, function ($reader) {
-        })->get();
-
-        //$data_import_column = [];
-        foreach ($rows as $value) {
-            $a = [];
-            foreach ($select_column as $sk => $s) {
-                $colname = $table_columns[$sk];
-
-                if (! CRUDBooster::isForeignKey($colname) || intval($value->$s)) {
-                    $a[$colname] = $value->$s;
-                    continue;
-                }
-
-                //Skip if value is empty
-                if ($value->$s == '') {
-                    continue;
-                }
-
-                $relation_table = CRUDBooster::getTableForeignKey($colname);
-                $relation_moduls = DB::table('cms_moduls')->where('table_name', $relation_table)->first();
-
-                $relation_class = __NAMESPACE__.'\\'.$relation_moduls->controller;
-                if (! class_exists($relation_class)) {
-                    $relation_class = '\App\Http\Controllers\\'.$relation_moduls->controller;
-                }
-                $relation_class = new $relation_class;
-                $relation_class->cbLoader();
-
-                $title_field = $relation_class->title_field;
-
-                $relation_insert_data = [];
-                $relation_insert_data[$title_field] = $value->$s;
-
-                if (CRUDBooster::isColumnExists($relation_table, 'created_at')) {
-                    $relation_insert_data['created_at'] = date('Y-m-d H:i:s');
-                }
-
-                try {
-                    $relation_exists = DB::table($relation_table)->where($title_field, $value->$s)->first();
-                    if ($relation_exists) {
-                        $relation_primary_key = $relation_class->primary_key;
-                        $relation_id = $relation_exists->$relation_primary_key;
-                    } else {
-                        $relation_id = DB::table($relation_table)->insertGetId($relation_insert_data);
-                    }
-
-                    $a[$colname] = $relation_id;
-                } catch (\Exception $e) {
-                    exit($e);
-                }
-            }
-
-            $has_title_field = true;
-            foreach ($a as $k => $v) {
-                if ($k == $this->title_field && $v == '') {
-                    $has_title_field = false;
-                    break;
-                }
-            }
-
-            if ($has_title_field == false) {
-                continue;
-            }
-
-            try {
-                if (CRUDBooster::isColumnExists($this->table, 'created_at')) {
-                    $a['created_at'] = date('Y-m-d H:i:s');
-                }
-
-                $this->table()->insert($a);
-                Cache::increment('success_'.$file_md5);
-            } catch (\Exception $e) {
-                $e = (string) $e;
-                Cache::put('error_'.$file_md5, $e, 500);
-            }
-        }
+        $import->InsertToDB($fileMD5, $this->table, $this->title_field);
 
         return response()->json(['status' => true]);
     }
