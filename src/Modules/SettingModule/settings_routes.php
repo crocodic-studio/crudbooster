cbd08939727f1edca01ae37bc7afed13955a7ce2
imanghafoori <imanghafoori1@gmail.com>
2017-11-02 04:32:52 -0400

    clean up

--- src/Modules/SettingModule/settings_routes.php
+++ src/Modules/SettingModule/settings_routes.php
@@ -8,29 +8,30 @@ Route::group([
     'namespace' => '\\crocodicstudio\\crudbooster\\Modules\\SettingModule',
 ], function () {
     Route::get('settings/', ['uses' => 'AdminSettingsController@getIndex', 'as' => 'AdminSettingsControllerGetIndex']);
-    Route::get('settings/show/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getShow', 'as' => 'AdminSettingsControllerGetShow']);
-    Route::get('settings/delete-file-setting/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getDeleteFileSetting', 'as' => 'AdminSettingsControllerGetDeleteFileSetting']);
-    Route::post('settings/save-setting/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postSaveSetting', 'as' => 'AdminSettingsControllerPostSaveSetting',]);
-    Route::get('settings/export-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getExportData', 'as' => 'AdminSettingsControllerGetExportData']);
-    Route::post('settings/export-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postExportData', 'as' => 'AdminSettingsControllerPostExportData',]);
-    Route::get('settings/data-query/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getDataQuery', 'as' => 'AdminSettingsControllerGetDataQuery']);
-    Route::get('settings/data-table/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getDataTable', 'as' => 'AdminSettingsControllerGetDataTable']);
-    Route::get('settings/data-modal-datatable/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getDataModalDatatable', 'as' => 'AdminSettingsControllerGetDataModalDatatable']);
-    Route::get('settings/update-single/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getUpdateSingle', 'as' => 'AdminSettingsControllerGetUpdateSingle']);
-    Route::post('settings/find-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postFindData', 'as' => 'AdminSettingsControllerPostFindData',]);
+    Route::get('settings/show', ['uses' => 'AdminSettingsController@getShow', 'as' => 'AdminSettingsControllerGetShow']);
+    Route::get('settings/delete-file-setting', ['uses' => 'AdminSettingsController@getDeleteFileSetting', 'as' => 'AdminSettingsControllerGetDeleteFileSetting']);
+    Route::get('settings/export-data', ['uses' => 'AdminSettingsController@getExportData', 'as' => 'AdminSettingsControllerGetExportData']);
+    Route::get('settings/data-query', ['uses' => 'AdminSettingsController@getDataQuery', 'as' => 'AdminSettingsControllerGetDataQuery']);
+    Route::get('settings/data-table', ['uses' => 'AdminSettingsController@getDataTable', 'as' => 'AdminSettingsControllerGetDataTable']);
+    Route::get('settings/data-modal-datatable', ['uses' => 'AdminSettingsController@getDataModalDatatable', 'as' => 'AdminSettingsControllerGetDataModalDatatable']);
+    Route::get('settings/update-single', ['uses' => 'AdminSettingsController@getUpdateSingle', 'as' => 'AdminSettingsControllerGetUpdateSingle']);
+    Route::get('settings/add', ['uses' => 'AdminSettingsController@getAdd', 'as' => 'AdminSettingsControllerGetAdd']);
+    Route::get('settings/edit/{id?}', ['uses' => 'AdminSettingsController@getEdit', 'as' => 'AdminSettingsControllerGetEdit']);
+    Route::get('settings/delete/{one?}', ['uses' => 'AdminSettingsController@getDelete', 'as' => 'AdminSettingsControllerGetDelete']);
+    Route::get('settings/detail/{one?}', ['uses' => 'AdminSettingsController@getDetail', 'as' => 'AdminSettingsControllerGetDetail']);
+    Route::get('settings/import-data', ['uses' => 'AdminSettingsController@getImportData', 'as' => 'AdminSettingsControllerGetImportData']);
+    Route::get('settings/delete-image', ['uses' => 'AdminSettingsController@getDeleteImage', 'as' => 'AdminSettingsControllerGetDeleteImage']);
+
     Route::post('settings/find-data-old/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postFindDataOld', 'as' => 'AdminSettingsControllerPostFindDataOld',]);
-    Route::get('settings/add/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getAdd', 'as' => 'AdminSettingsControllerGetAdd']);
-    Route::post('settings/add-save/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postAddSave', 'as' => 'AdminSettingsControllerPostAddSave',]);
-    Route::get('settings/edit/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getEdit', 'as' => 'AdminSettingsControllerGetEdit']);
-    Route::post('settings/edit-save/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postEditSave', 'as' => 'AdminSettingsControllerPostEditSave',]);
-    Route::get('settings/delete/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getDelete', 'as' => 'AdminSettingsControllerGetDelete']);
-    Route::get('settings/detail/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getDetail', 'as' => 'AdminSettingsControllerGetDetail']);
-    Route::get('settings/import-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getImportData', 'as' => 'AdminSettingsControllerGetImportData']);
     Route::post('settings/done-import/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postDoneImport', 'as' => 'AdminSettingsControllerPostDoneImport',]);
+    Route::post('settings/find-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postFindData', 'as' => 'AdminSettingsControllerPostFindData',]);
+    Route::post('settings/save-setting/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postSaveSetting', 'as' => 'AdminSettingsControllerPostSaveSetting',]);
+    Route::post('settings/export-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postExportData', 'as' => 'AdminSettingsControllerPostExportData',]);
     Route::post('settings/do-import-chunk/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postDoImportChunk', 'as' => 'AdminSettingsControllerPostDoImportChunk',]);
     Route::post('settings/do-upload-import-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postDoUploadImportData', 'as' => 'AdminSettingsControllerPostDoUploadImportData',]);
     Route::post('settings/action-selected/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postActionSelected', 'as' => 'AdminSettingsControllerPostActionSelected',]);
-    Route::get('settings/delete-image/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@getDeleteImage', 'as' => 'AdminSettingsControllerGetDeleteImage']);
+    Route::post('settings/add-save/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postAddSave', 'as' => 'AdminSettingsControllerPostAddSave',]);
     Route::post('settings/upload-summernote/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postUploadSummernote', 'as' => 'AdminSettingsControllerPostUploadSummernote',]);
+    Route::post('settings/edit-save/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postEditSave', 'as' => 'AdminSettingsControllerPostEditSave',]);
     Route::post('settings/upload-file/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postUploadFile', 'as' => 'AdminSettingsControllerPostUploadFile',]);
 });
