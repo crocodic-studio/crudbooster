0cbd682059ffd45f67ab1105b08c8643646d29da
imanghafoori <imanghafoori1@gmail.com>
2017-11-02 05:09:56 -0400

    clean up routes

--- src/Modules/MenuModule/menus_routes.php
+++ src/Modules/MenuModule/menus_routes.php
@@ -6,27 +6,28 @@ Route::group([
     'namespace' => '\crocodicstudio\crudbooster\Modules\MenuModule',
 ], function () {
     Route::get('menus/', ['uses' => 'AdminMenusController@getIndex', 'as' => 'AdminMenusControllerGetIndex']);
-    Route::post('menus/save-menu/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postSaveMenu', 'as' => 'AdminMenusControllerPostSaveMenu',]);
-    Route::get('menus/export-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getExportData', 'as' => 'AdminMenusControllerGetExportData']);
-    Route::post('menus/export-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postExportData', 'as' => 'AdminMenusControllerPostExportData',]);
+    Route::get('menus/export-data', ['uses' => 'AdminMenusController@getExportData', 'as' => 'AdminMenusControllerGetExportData']);
     Route::get('menus/data-query/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getDataQuery', 'as' => 'AdminMenusControllerGetDataQuery']);
-    Route::get('menus/data-table/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getDataTable', 'as' => 'AdminMenusControllerGetDataTable']);
+    Route::get('menus/data-table', ['uses' => 'AdminMenusController@getDataTable', 'as' => 'AdminMenusControllerGetDataTable']);
     Route::get('menus/data-modal-datatable/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getDataModalDatatable', 'as' => 'AdminMenusControllerGetDataModalDatatable']);
-    Route::get('menus/update-single/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getUpdateSingle', 'as' => 'AdminMenusControllerGetUpdateSingle']);
-    Route::post('menus/find-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postFindData', 'as' => 'AdminMenusControllerPostFindData',]);
-    Route::post('menus/find-data-old/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postFindDataOld', 'as' => 'AdminMenusControllerPostFindDataOld',]);
+    Route::get('menus/update-single', ['uses' => 'AdminMenusController@getUpdateSingle', 'as' => 'AdminMenusControllerGetUpdateSingle']);
     Route::get('menus/add/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getAdd', 'as' => 'AdminMenusControllerGetAdd']);
+    Route::get('menus/edit/{one?}', ['uses' => 'AdminMenusController@getEdit', 'as' => 'AdminMenusControllerGetEdit']);
+    Route::get('menus/delete/{id?}', ['uses' => 'AdminMenusController@getDelete', 'as' => 'AdminMenusControllerGetDelete']);
+    Route::get('menus/detail/{one?}', ['uses' => 'AdminMenusController@getDetail', 'as' => 'AdminMenusControllerGetDetail']);
+    Route::get('menus/import-data', ['uses' => 'AdminMenusController@getImportData', 'as' => 'AdminMenusControllerGetImportData']);
+    Route::get('menus/delete-image', ['uses' => 'AdminMenusController@getDeleteImage', 'as' => 'AdminMenusControllerGetDeleteImage']);
+
+    Route::post('menus/save-menu/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postSaveMenu', 'as' => 'AdminMenusControllerPostSaveMenu',]);
+    Route::post('menus/export-data', ['uses' => 'AdminMenusController@postExportData', 'as' => 'AdminMenusControllerPostExportData',]);
+    Route::post('menus/find-data-old/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postFindDataOld', 'as' => 'AdminMenusControllerPostFindDataOld',]);
     Route::post('menus/add-save/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postAddSave', 'as' => 'AdminMenusControllerPostAddSave',]);
-    Route::get('menus/edit/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getEdit', 'as' => 'AdminMenusControllerGetEdit']);
-    Route::post('menus/edit-save/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postEditSave', 'as' => 'AdminMenusControllerPostEditSave',]);
-    Route::get('menus/delete/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getDelete', 'as' => 'AdminMenusControllerGetDelete']);
-    Route::get('menus/detail/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getDetail', 'as' => 'AdminMenusControllerGetDetail']);
-    Route::get('menus/import-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getImportData', 'as' => 'AdminMenusControllerGetImportData']);
-    Route::post('menus/done-import/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postDoneImport', 'as' => 'AdminMenusControllerPostDoneImport',]);
-    Route::post('menus/do-import-chunk/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postDoImportChunk', 'as' => 'AdminMenusControllerPostDoImportChunk',]);
-    Route::post('menus/do-upload-import-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postDoUploadImportData', 'as' => 'AdminMenusControllerPostDoUploadImportData',]);
+    Route::post('menus/edit-save/{one?}', ['uses' => 'AdminMenusController@postEditSave', 'as' => 'AdminMenusControllerPostEditSave',]);
+    Route::post('menus/find-data', ['uses' => 'AdminMenusController@postFindData', 'as' => 'AdminMenusControllerPostFindData',]);
+    Route::post('menus/done-import', ['uses' => 'AdminMenusController@postDoneImport', 'as' => 'AdminMenusControllerPostDoneImport',]);
+    Route::post('menus/do-import-chunk', ['uses' => 'AdminMenusController@postDoImportChunk', 'as' => 'AdminMenusControllerPostDoImportChunk',]);
+    Route::post('menus/do-upload-import-data', ['uses' => 'AdminMenusController@postDoUploadImportData', 'as' => 'AdminMenusControllerPostDoUploadImportData',]);
     Route::post('menus/action-selected/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postActionSelected', 'as' => 'AdminMenusControllerPostActionSelected',]);
-    Route::get('menus/delete-image/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@getDeleteImage', 'as' => 'AdminMenusControllerGetDeleteImage']);
     Route::post('menus/upload-summernote/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postUploadSummernote', 'as' => 'AdminMenusControllerPostUploadSummernote',]);
     Route::post('menus/upload-file/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminMenusController@postUploadFile', 'as' => 'AdminMenusControllerPostUploadFile',]);
 });
--- src/Modules/SettingModule/settings_routes.php
+++ src/Modules/SettingModule/settings_routes.php
@@ -23,15 +23,15 @@ Route::group([
     Route::get('settings/delete-image', ['uses' => 'AdminSettingsController@getDeleteImage', 'as' => 'AdminSettingsControllerGetDeleteImage']);
 
     Route::post('settings/find-data-old/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postFindDataOld', 'as' => 'AdminSettingsControllerPostFindDataOld',]);
-    Route::post('settings/done-import/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postDoneImport', 'as' => 'AdminSettingsControllerPostDoneImport',]);
-    Route::post('settings/find-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postFindData', 'as' => 'AdminSettingsControllerPostFindData',]);
+    Route::post('settings/done-import', ['uses' => 'AdminSettingsController@postDoneImport', 'as' => 'AdminSettingsControllerPostDoneImport',]);
+    Route::post('settings/find-data', ['uses' => 'AdminSettingsController@postFindData', 'as' => 'AdminSettingsControllerPostFindData',]);
     Route::post('settings/save-setting/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postSaveSetting', 'as' => 'AdminSettingsControllerPostSaveSetting',]);
-    Route::post('settings/export-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postExportData', 'as' => 'AdminSettingsControllerPostExportData',]);
-    Route::post('settings/do-import-chunk/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postDoImportChunk', 'as' => 'AdminSettingsControllerPostDoImportChunk',]);
-    Route::post('settings/do-upload-import-data/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postDoUploadImportData', 'as' => 'AdminSettingsControllerPostDoUploadImportData',]);
+    Route::post('settings/export-data', ['uses' => 'AdminSettingsController@postExportData', 'as' => 'AdminSettingsControllerPostExportData',]);
+    Route::post('settings/do-import-chunk', ['uses' => 'AdminSettingsController@postDoImportChunk', 'as' => 'AdminSettingsControllerPostDoImportChunk',]);
+    Route::post('settings/do-upload-import-data', ['uses' => 'AdminSettingsController@postDoUploadImportData', 'as' => 'AdminSettingsControllerPostDoUploadImportData',]);
     Route::post('settings/action-selected/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postActionSelected', 'as' => 'AdminSettingsControllerPostActionSelected',]);
     Route::post('settings/add-save/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postAddSave', 'as' => 'AdminSettingsControllerPostAddSave',]);
     Route::post('settings/upload-summernote/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postUploadSummernote', 'as' => 'AdminSettingsControllerPostUploadSummernote',]);
-    Route::post('settings/edit-save/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postEditSave', 'as' => 'AdminSettingsControllerPostEditSave',]);
+    Route::post('settings/edit-save/{one?}', ['uses' => 'AdminSettingsController@postEditSave', 'as' => 'AdminSettingsControllerPostEditSave',]);
     Route::post('settings/upload-file/{one?}/{two?}/{three?}/{four?}/{five?}', ['uses' => 'AdminSettingsController@postUploadFile', 'as' => 'AdminSettingsControllerPostUploadFile',]);
 });
