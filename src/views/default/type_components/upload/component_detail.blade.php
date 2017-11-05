ba87778c3ef1a9c147ba329fbbb19a2283217f32
imanghafoori <imanghafoori1@gmail.com>
2017-11-06 00:42:39 +0330

    clean up

--- src/views/default/type_components/upload/component_detail.blade.php
+++ src/views/default/type_components/upload/component_detail.blade.php
@@ -1,12 +1,16 @@
 <?php
 $imagesExtension = explode(',', cbConfig('IMAGE_EXTENSIONS'));
 $ext = pathinfo($value, PATHINFO_EXTENSION);
-if(Storage::exists($value)):
-if(in_array($ext, $imagesExtension)):?>
-<a data-lightbox='roadtrip' href='{{asset($value)}}'><img style='max-width:150px' title="Image For {{$form['label']}}"
-                                                          src='{{asset($value)}}'/></a>
-<?php else:?>
-<a href='{{asset($value)}}?download=1' title="File For {{$form['label']}}"
-   target="_blank">{{cbTrans("button_download_file")}}</a>
-<?php endif;?>
-<?php endif;?>
+?>
+@if(Storage::exists($value))
+    @if(in_array($ext, $imagesExtension))
+        <a data-lightbox='roadtrip' href='{{asset($value)}}'>
+            <img style='max-width:150px' title="Image For {{$form['label']}}" src='{{asset($value)}}'/>
+        </a>
+    @else
+        <a href='{{asset($value)}}?download=1'
+           title="File For {{$form['label']}}"
+           target="_blank">{{cbTrans("button_download_file")}}
+        </a>
+    @endif
+@endif
