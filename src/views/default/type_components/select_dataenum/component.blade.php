7ef19a25c7b7f00d2cd7b82456826b6c14762818
imanghafoori <imanghafoori1@gmail.com>
2017-11-06 00:34:49 +0330

    clean up

--- src/views/default/type_components/select_dataenum/component.blade.php
+++ src/views/default/type_components/select_dataenum/component.blade.php
@@ -1,4 +1,15 @@
 <?php $default = ! empty($form['placeholder']) ? $form['placeholder'] : cbTrans('text_prefix_option')." ".$form['label'];?>
+
+<?php
+$options = [];
+$enumValue = $form['options']['value'];
+foreach ($form['options']['enum'] as $i => $e) {
+    $options[$i]['label'] = $e;
+    $options[$i]['value'] = ($enumValue) ? $enumValue[$i] : $e;
+    $options[$i]['select'] = ($value && $value == $options[$i]['value']) ? "selected" : "";
+}
+?>
+
 <div class='form-group {{$header_group_class}} {{ ($errors->first($name))?"has-error":"" }}' id='form-group-{{$name}}'
      style="{{@$form['style']}}">
     <label class='control-label col-sm-2'>{{$form['label']}} {!!($required)?"<span class='text-danger' title='This field is required'>*</span>":"" !!}</label>
@@ -7,15 +18,9 @@
         <select class='form-control' id="{{$name}}" data-value='{{$value}}'
                 {{$required}} {!!$placeholder!!} {{$readonly}} {{$disabled}} name="{{$name}}">
             <option value=''>{{$default}}</option>
-            <?php
-            $enum = $form['options']['enum'];
-            $enumValue = $form['options']['value'];
-            foreach ($enum as $i => $e) {
-                $v = ($enumValue) ? $enumValue[$i] : $e;
-                $select = ($value && $value == $v) ? "selected" : "";
-                echo "<option $select value='$v'>$e</option>";
-            }
-            ?>
+            @foreach($options as $option)
+                <option {!! $option['select'] !!} value='{!! $option['value'] !!}'>{!! $option['label'] !!}</option>
+            @endforeach
         </select>
         <div class="text-danger">{!! $errors->first($name)?"<i class='fa fa-info-circle'></i> ".$errors->first($name):"" !!}</div>
         <p class='help-block'>{{ @$form['help'] }}</p>
