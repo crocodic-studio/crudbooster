db1ed5d53b9e307fdf842019d33f3fc58432519c
imanghafoori <imanghafoori1@gmail.com>
2017-11-06 15:45:54 -0500

    extract partial

--- src/views/default/_index/advanced_filter_modal.blade.php
+++ src/views/default/_index/advanced_filter_modal.blade.php
@@ -32,49 +32,20 @@
 
                                 <div class='row between-group'
                                      style="{{ (CRUDBooster::getTypeFilter($col["field_with"]) == 'between')?"display:block":"display:none" }}">
-                                    <div class='col-sm-6'>
-                                        <div class='input-group {{ ($col["type_data"] == "time")?"bootstrap-timepicker":"" }}'>
-                                            <span class="input-group-addon">{{cbTrans("filter_from")}}:</span>
-                                            <input
-                                                    {{ (CRUDBooster::getTypeFilter($col["field_with"]) != 'between')?"disabled":"" }}
-                                                    type='text'
-                                                    class='filter-value-between form-control {{ (in_array($col["type_data"],["date","datetime","timestamp"]))?"datepicker":"timepicker" }}'
-                                                    readonly
-                                                    placeholder='{{$col["label"]}} {{cbTrans("filter_from")}}'
-                                                    name='filter_column[{{$col["field_with"]}}][value][]'
-                                                    value='<?php
-                                                    $value = CRUDBooster::getValueFilter($col["field_with"]);
-                                                    echo (CRUDBooster::getTypeFilter($col["field_with"]) == 'between') ? $value[0] : "";
-                                                    ?>'>
-                                        </div>
-                                    </div>
-                                    <div class='col-sm-6'>
-                                        <div class='input-group {{ ($col["type_data"] == "time")?"bootstrap-timepicker":"" }}'>
-                                            <span class="input-group-addon">{{cbTrans("filter_to")}}:</span>
-                                            <input
-                                                    {{ (CRUDBooster::getTypeFilter($col["field_with"]) != 'between')?"disabled":"" }}
-                                                    type='text'
-                                                    class='filter-value-between form-control {{ (in_array($col["type_data"],["date","datetime","timestamp"]))?"datepicker":"timepicker" }}'
-                                                    readonly
-                                                    placeholder='{{$col["label"]}} {{cbTrans("filter_to")}}'
-                                                    name='filter_column[{{$col["field_with"]}}][value][]'
-                                                    value='<?php
-                                                    $value = CRUDBooster::getValueFilter($col["field_with"]);
-                                                    echo (CRUDBooster::getTypeFilter($col["field_with"]) == 'between') ? $value[1] : "";
-                                                    ?>'>
-                                        </div>
-                                    </div>
+
+                                    <?php $value = CRUDBooster::getValueFilter($col["field_with"]); ?>
+
+                                    @include('crudbooster::default._index.advanced_filter_modal.datePicker',
+                                        ['dir' => 'from', 'value' => $value[0]] + array_only($col, ['field_with', 'type_data', 'label']))
+
+                                    @include('crudbooster::default._index.advanced_filter_modal.datePicker',
+                                        ['dir' => 'to',   'value' => $value[1]] + array_only($col, ['field_with', 'type_data', 'label']))
+
                                 </div>
                             </div><!--END COL_SM_6-->
 
 
-                            <div class='col-sm-2'>
-                                <select class='form-control' name='filter_column[{{$col["field_with"]}}][sorting]'>
-                                    <option value=''>** Sorting</option>
-                                    <option {{ (CRUDBooster::getSortingFilter($col["field_with"]) == 'asc')?"selected":"" }} value='asc'>{{cbTrans("filter_ascending")}}</option>
-                                    <option {{ (CRUDBooster::getSortingFilter($col["field_with"]) == 'desc')?"selected":"" }} value='desc'>{{cbTrans("filter_descending")}}</option>
-                                </select>
-                            </div><!--END_COL_SM_2-->
+                            @include('crudbooster::default._index.advanced_filter_modal.sort', ['field_with' => $col['field_with']])
 
                         </div>
 
@@ -82,13 +53,11 @@
                     @endforeach
 
                 </div>
-                <div class="modal-footer" align="right">
-                    <button class="btn btn-default" type="button"
-                            data-dismiss="modal">{{cbTrans("button_close")}}</button>
-                    <button class="btn btn-default btn-reset" type="reset"
-                            onclick='location.href="{{Request::get("lasturl")}}"'>{{cbTrans("button_reset")}}</button>
-                    <button class="btn btn-primary btn-submit" type="submit">{{cbTrans("button_submit")}}</button>
-                </div>
+
+
+                @include('crudbooster::default._index.advanced_filter_modal.footer')
+
+
                 <input type="hidden" name="lasturl" value="{{Request::get('lasturl')?:Request::fullUrl()}}">
             </form>
         </div>
