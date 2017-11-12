044997af45686bc6cbca0cc8a1e53eb1fe187871
imanghafoori <imanghafoori1@gmail.com>
2017-11-12 15:51:40 +0330

    register routes

--- src/Modules/EmailTemplates/email_templates_routes.php
+++ src/Modules/EmailTemplates/email_templates_routes.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Iman
- * Date: 11/12/2017
- * Time: 2:36 PM
- */
+Route::group([
+    'middleware' => ['web', \crocodicstudio\crudbooster\middlewares\CBSuperadmin::class],
+    'prefix' => cbAdminPath(),
+    'namespace' => cbModulesNS('EmailTemplates'),
+], function (){
+    Route::get('email-templates/', ['uses' => 'AdminEmailTemplatesController@getIndex', 'as' => 'AdminEmailTemplatesControllerGetIndex']);
+    Route::get('email-templates/data-query', ['uses' => 'AdminEmailTemplatesController@getDataQuery', 'as' => 'AdminEmailTemplatesControllerGetDataQuery']);
+    Route::get('email-templates/data-table', ['uses' => 'AdminEmailTemplatesController@getDataTable', 'as' => 'AdminEmailTemplatesControllerGetDataTable']);
+    Route::get('email-templates/data-modal-datatable', ['uses' => 'AdminEmailTemplatesController@getDataModalDatatable', 'as' => 'AdminEmailTemplatesControllerGetDataModalDatatable']);
+    Route::get('email-templates/update-single', ['uses' => 'AdminEmailTemplatesController@getUpdateSingle', 'as' => 'AdminEmailTemplatesControllerGetUpdateSingle']);
+    Route::get('email-templates/add', ['uses' => 'AdminEmailTemplatesController@getAdd', 'as' => 'AdminEmailTemplatesControllerGetAdd']);
+    Route::get('email-templates/edit/{id?}', ['uses' => 'AdminEmailTemplatesController@getEdit', 'as' => 'AdminEmailTemplatesControllerGetEdit']);
+    Route::get('email-templates/delete/{id?}', ['uses' => 'AdminEmailTemplatesController@getDelete', 'as' => 'AdminEmailTemplatesControllerGetDelete']);
+    Route::get('email-templates/detail/{id?}', ['uses' => 'AdminEmailTemplatesController@getDetail', 'as' => 'AdminEmailTemplatesControllerGetDetail']);
+    Route::get('email-templates/delete-image/{id?}', ['uses' => 'AdminEmailTemplatesController@getDeleteImage', 'as' => 'AdminEmailTemplatesControllerGetDeleteImage']);
+
+    Route::post('email-templates/export-data', [ 'uses' => 'AdminEmailTemplatesController@postExportData', 'as' => 'AdminEmailTemplatesControllerPostExportData',]);
+    Route::post('email-templates/find-data', ['uses' => 'AdminEmailTemplatesController@postFindData', 'as' => 'AdminEmailTemplatesControllerPostFindData',]);
+    Route::post('email-templates/find-data-old', ['uses' => 'AdminEmailTemplatesController@postFindDataOld', 'as' => 'AdminEmailTemplatesControllerPostFindDataOld',]);
+    Route::post('email-templates/add-save', ['uses' => 'AdminEmailTemplatesController@postAddSave', 'as' => 'AdminEmailTemplatesControllerPostAddSave',]);
+    Route::post('email-templates/edit-save/{id?}', ['uses' => 'AdminEmailTemplatesController@postEditSave', 'as' => 'AdminEmailTemplatesControllerPostEditSave',]);
+    Route::post('email-templates/do-import-chunk', ['uses' => 'AdminEmailTemplatesController@postDoImportChunk', 'as' => 'AdminEmailTemplatesControllerPostDoImportChunk',]);
+    Route::post('email-templates/action-selected', ['uses' => 'AdminEmailTemplatesController@postActionSelected', 'as' => 'AdminEmailTemplatesControllerPostActionSelected',]);
+});
