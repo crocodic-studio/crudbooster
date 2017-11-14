2028fcede9d4c1a3065d6ea034cee650a0402315
Adrian Lim <adrian.lbs@gmail.com>
2017-11-14 16:56:53 +0800

    Updated date items

--- src/views/statistic_builder/index.blade.php
+++ src/views/statistic_builder/index.blade.php
@@ -331,9 +331,20 @@
 
     <div id='statistic-area'>
 
-        <div class="input-group">                               
-            <span class="input-group-addon open-datetimepicker"><a><i class='fa fa-calendar '></i></a></span>
-            <input type='text' title="Date From" readonly class='form-control notfocus input_date' name="testdate1" id="testdate1" value='1970-01-01 00:00:00'/>                       
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="input-group">                               
+                    <span class="input-group-addon open-datetimepicker"><a><i class='fa fa-calendar '></i></a></span>
+                    <input type='text' title="Date From" readonly class='form-control notfocus input_date' name="testdate1" id="testdate1" value='1970-01-01 00:00:00'/>                       
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="input-group">                               
+                    <span class="input-group-addon open-datetimepicker"><a><i class='fa fa-calendar '></i></a></span>
+                    <input type='text' title="Date From" readonly class='form-control notfocus input_date' name="testdate2" id="testdate2" value='1970-01-01 00:00:00'/>                       
+                </div>
+            </div>
+        
         </div>
         <div class="statistic-row row">
             <div id='area1' class="col-sm-3 connectedSortable">            	
