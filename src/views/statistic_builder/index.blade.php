22ae34fbcb39f70b50c7be71bd4e44f63d1709e0
Adrian Lim <adrian.lbs@gmail.com>
2017-11-15 00:52:35 +0800

    Fixed val item

--- src/views/statistic_builder/index.blade.php
+++ src/views/statistic_builder/index.blade.php
@@ -7,7 +7,7 @@
     <script type="text/javascript">
         var lang = '{{App::getLocale()}}';
         $(function() {
-            $('#testdate2').value(new Date().toJSON().slice(0,10));
+            $('#testdate2').val(new Date().toJSON().slice(0,10));
             $('.input_date').datepicker({
                 dateFormat: 'yy-mm-dd',
                 @if (App::getLocale() == 'ar')
