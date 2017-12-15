5297e1da024b98678c8f61e1479136689a4b84e8
Adrian Lim <adrian.lbs@gmail.com>
2017-12-15 13:04:59 +0800

    Added more pie chart padding text

--- src/views/statistic_builder/components/chartpie.blade.php
+++ src/views/statistic_builder/components/chartpie.blade.php
@@ -121,6 +121,67 @@
     	</div>
 		
 		<script type="text/javascript">
+        function getBoxWidth(labelOpts, fontSize) {
+          return labelOpts.usePointStyle ?
+            fontSize * Math.SQRT2 :
+          labelOpts.boxWidth;
+        };
+
+        Chart.NewLegend = Chart.Legend.extend({
+          afterFit: function() {
+            this.height = this.height + 50;
+          },
+        });
+
+        function createNewLegendAndAttach(chartInstance, legendOpts) {
+          var legend = new Chart.NewLegend({
+            ctx: chartInstance.chart.ctx,
+            options: legendOpts,
+            chart: chartInstance
+          });
+          
+          if (chartInstance.legend) {
+            Chart.layoutService.removeBox(chartInstance, chartInstance.legend);
+            delete chartInstance.newLegend;
+          }
+          
+          chartInstance.newLegend = legend;
+          Chart.layoutService.addBox(chartInstance, legend);
+        }
+
+        // Register the legend plugin
+        Chart.plugins.register({
+          beforeInit: function(chartInstance) {
+            var legendOpts = chartInstance.options.legend;
+
+            if (legendOpts) {
+              createNewLegendAndAttach(chartInstance, legendOpts);
+            }
+          },
+          beforeUpdate: function(chartInstance) {
+            var legendOpts = chartInstance.options.legend;
+
+            if (legendOpts) {
+              legendOpts = Chart.helpers.configMerge(Chart.defaults.global.legend, legendOpts);
+
+              if (chartInstance.newLegend) {
+                chartInstance.newLegend.options = legendOpts;
+              } else {
+                createNewLegendAndAttach(chartInstance, legendOpts);
+              }
+            } else {
+              Chart.layoutService.removeBox(chartInstance, chartInstance.newLegend);
+              delete chartInstance.newLegend;
+            }
+          },
+          afterEvent: function(chartInstance, e) {
+            var legend = chartInstance.newLegend;
+            if (legend) {
+              legend.handleEvent(e);
+            }
+          }
+        });
+        
         var color = Chart.helpers.color;
         window.chartColors = {
             red: '#FF4136',
@@ -230,14 +291,6 @@
                 //type: 'pie',
                 data: barChartData,
                 options: {
-                    layout: {
-                            padding: {
-                                    left: 150,
-                                    right: 150,
-                                    top: 150,
-                                    bottom: 150,
-                            },
-                    },
                     responsive: true,
                     legend: {
                         position: 'top',
