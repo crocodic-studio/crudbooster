8ce544d6b2f3dfed2117f10df9d54889617217e9
Samuele <samuele@busnet.it>
2018-01-09 17:20:13 +0100

    Italian translation added

---
+++ src/localization/it/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La password deve essere almeno di 6 caratteri e deve corrispondere alla conferma.',
+    'reset' => 'La tua password è stata resettata!',
+    'sent' => 'Ti è stato inviato via mail il link di reset!',
+    'token' => 'Token di reset password non valido.',
+    'user' => "Non troviamo un utente con la e-mail inserita.",
+
+];
