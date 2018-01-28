66810e94bffc90a729ad9d53f8b441a55788b54b
Adrian Lim <adrian.lbs@gmail.com>
2018-01-28 20:31:12 +0800

    Fixed jquery css sizing issue

--- src/views/statistic_builder/pdf.blade.php
+++ src/views/statistic_builder/pdf.blade.php
@@ -16,7 +16,6 @@
     <style type="text/css">
 
         body {
-		  background: rgb(204,204,204); 
 		}
 		page[size="A4"] {
 		  background: white;
@@ -25,7 +24,6 @@
 		  display: block;
 		  margin: 0 auto;
 		  margin-bottom: 0.5cm;
-		  box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
 		}
 		@media print {
 		  body, page[size="A4"] {
