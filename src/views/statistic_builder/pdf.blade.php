68c3ee810f4aa102ec5350a5f3c72c7b98d93bb1
Adrian Lim <adrian.lbs@gmail.com>
2018-01-28 19:43:06 +0800

    Setup for page sizing

--- src/views/statistic_builder/pdf.blade.php
+++ src/views/statistic_builder/pdf.blade.php
@@ -70,6 +70,25 @@
         }
 
         .ui-datepicker{ z-index: 99999 !important;}
+
+        body {
+		  background: rgb(204,204,204); 
+		}
+		page[size="A4"] {
+		  background: white;
+		  width: 21cm;
+		  height: 29.7cm;
+		  display: block;
+		  margin: 0 auto;
+		  margin-bottom: 0.5cm;
+		  box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+		}
+		@media print {
+		  body, page[size="A4"] {
+		    margin: 0;
+		    box-shadow: 0;
+		  }
+		}
     </style>
 
 	<!-- ADDITION FUNCTION FOR BUTTON -->
