ead2852fb57989d2b6454737737f13d06d12f026
Adrian Lim <adrian.lbs@gmail.com>
2018-01-28 17:52:33 +0800

    Fixed issue with statistic builder

--- src/views/statistic_builder/show.blade.php
+++ src/views/statistic_builder/show.blade.php
@@ -1,6 +1,6 @@
 @extends($ispdf?'crudbooster::admin_template_nosidebar':'crudbooster::statistic_builder.index')
 @section('content') 
 	
-	@include($ispdf'crudbooster::statistic_builder.pdf':'crudbooster::statistic_builder.index')
+	@include($ispdf?'crudbooster::statistic_builder.pdf':'crudbooster::statistic_builder.index')
 
 @endsection
