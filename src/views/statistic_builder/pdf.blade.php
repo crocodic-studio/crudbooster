c22964930236994da886bacb2b15a5c7339899a8
Adrian Lim <adrian.lbs@gmail.com>
2018-01-28 21:36:38 +0800

    Remove add etc and added console.log to get total count

--- src/views/statistic_builder/pdf.blade.php
+++ src/views/statistic_builder/pdf.blade.php
@@ -116,6 +116,7 @@
         function runSortables() 
         {
         	var id_cms_statistics = '{{$id_cms_statistics}}';
+        	var totalcount = 0;
             /*if ($('#txtDateRange').text()=='All')
             {
                 var viewlink = "{{CRUDBooster::mainpath('view-component')}}/";
@@ -129,16 +130,16 @@
             var viewlink = "{{CRUDBooster::mainpath('view-component')}}/";
             var addon = "";
             $('.connectedSortable').each(function() {
-            	console.log("in sortable");
                 var areaname = $(this).attr('id');
+
                 
                 $.get("{{CRUDBooster::mainpath('list-component')}}/"+id_cms_statistics+"/"+areaname,function(response) {            
                     if(response.components) {
-                        
+                		totalcount = totalcount + response.components.length;
+                		console.log(totalcount);        
                         $.each(response.components,function(i,obj) {
                             $('#'+areaname).append("<div id='area-loading-"+obj.componentID+"' class='area-loading'><i class='fa fa-spin fa-spinner'></i></div>");
-                            $.get(viewlink+obj.componentID+addon,function(view) {
-                                console.log('View For CID '+view.componentID);
+                            $.get(viewlink+obj.componentID+addon,function(view) {                                
                                 $('#area-loading-'+obj.componentID).remove();
                                 $('#'+areaname).append(view.layout);
                                 
@@ -151,110 +152,9 @@
 
         $(function() {
 
-
-            var d = new Date();
-            d.setMonth(d.getMonth() - 1);
-            $('#testdate1').val(d.toJSON().slice(0,10));
-        	$('#testdate2').val(new Date().toJSON().slice(0,10));
-            $('.input_date').datepicker({
-                dateFormat: 'yy-mm-dd',
-                @if (App::getLocale() == 'ar')
-                rtl: true,
-                @endif
-                language: lang,
-                changeYear: true,
-                changeMonth: true,
-                onSelect: function(dateText) {
-                    $('.connectedSortable').empty();
-                    runSortables();
-                  }
-            });
-            
-            $('.open-datetimepicker').click(function() {
-                  $(this).next('.input_date').datepicker('show');
-            });
-
         	
         	runSortables();
             
-            $(document).on('click','.btn-delete-component',function() {
-            	var componentID = $(this).data('componentid');
-            	var $this = $(this);
-
-            	swal({
-				  title: "Are you sure?",
-				  text: "You will not be able to recover this widget !",
-				  type: "warning",
-				  showCancelButton: true,
-				  confirmButtonColor: "#DD6B55",
-				  confirmButtonText: "Yes",
-				  closeOnConfirm: true
-				},
-				function(){
-				  	
-	            	$.get("{{CRUDBooster::mainpath('delete-component')}}/"+componentID,function() {
-	            		$this.parents('.border-box').remove();
-	            		
-	            	});
-				});
-            	
-            })
-            $(document).on('click','.btn-edit-component',function() {
-				var componentID = $(this).data('componentid');
-				var name        = $(this).data('name');
-
-            	$('#modal-statistic .modal-title').text(name);
-            	$('#modal-statistic .modal-body').html("<i class='fa fa-spin fa-spinner'></i> Please wait loading...");
-            	$('#modal-statistic').modal('show');
-
-            	$.get("{{CRUDBooster::mainpath('edit-component')}}/"+componentID,function(response) {
-                    $('#modal-statistic .modal-body').html(response);
-                })
-            })
-
-            $('#modal-statistic .btn-submit').click(function() {         
-                
-                $('#modal-statistic form .has-error').removeClass('has-error');
-
-                var required_input = [];
-                $('#modal-statistic form').find('input[required],textarea[required],select[required]').each(function() {
-                    var $input = $(this);
-                    var $form_group = $input.parent('.form-group');
-                    var value = $input.val();
-
-                    if(value == '') {
-                        required_input.push($input.attr('name'));
-                    }
-                })    
-
-                if(required_input.length) {  
-                    setTimeout(function() {
-                        $.each(required_input,function(i,name) {
-                            $('#modal-statistic form').find('input[name="'+name+'"],textarea[name="'+name+'"],select[name="'+name+'"]').parent('.form-group').addClass('has-error');
-                        })
-                    },200);                  
-                    
-                    return false;
-                }
-
-            	var $button = $(this).text('Saving...').addClass('disabled');
-            	
-            	$.ajax({
-            		data:$('#modal-statistic form').serialize(),
-            		type:'POST',
-            		url:"{{CRUDBooster::mainpath('save-component')}}",
-            		success:function() {
-            			
-            			$button.removeClass('disabled').text('Save Changes');
-            			$('#modal-statistic').modal('hide');
-            			window.location.href = "{{Request::fullUrl()}}";
-            		},
-            		error:function() {
-            			alert('Sorry something went wrong !');
-            			$button.removeClass('disabled').text('Save Changes');
-            		}
-            	})
-            })
         })
     </script>
     
