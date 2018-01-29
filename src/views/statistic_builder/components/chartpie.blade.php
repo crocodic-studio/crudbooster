f0b5a8fbf8628beec2fc2f42cb9ce2d258c5b35d
Adrian Lim <adrian.lbs@gmail.com>
2018-01-29 10:12:08 +0800

    Added fix for charts

--- src/views/statistic_builder/components/chartpie.blade.php
+++ src/views/statistic_builder/components/chartpie.blade.php
@@ -295,7 +295,7 @@
                 options: {
                     responsive: true,
                     legend: {
-                        position: 'top',
+                        display:true
                     },
                     title: {
                         display: false,
@@ -303,14 +303,14 @@
                     },
                     tooltipEvents: [],
                     showTooltips: true,
-                    /*onAnimationComplete: function() {
+                    onAnimationComplete: function() {
                         this.showTooltip(this.segments, true);
                     },
                     pieceLabel: {
                         render: 'percentage',
                         fontColor: '#000',
                         position: 'outside'
-                    }*/
+                    }
                 },
                 
             });	
