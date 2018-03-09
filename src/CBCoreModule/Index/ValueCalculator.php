8dffe93916347bcbb5672808380f53f651d943db
imanghafoori <imanghafoori1@gmail.com>
2018-03-09 18:26:19 +0330

    refactor into private methods

--- src/CBCoreModule/Index/ValueCalculator.php
+++ src/CBCoreModule/Index/ValueCalculator.php
@@ -16,28 +16,11 @@ class ValueCalculator
         $title = @$row->{$this->cb->title_field};
         $label = $col['label'];
 
-        if (isset($col['image'])) {
-            if ($value == '') {
-                $value = "<a  data-lightbox='roadtrip' rel='group_{{$table}}' title='$label: $title' href='".asset('vendor/crudbooster/avatar.jpg')."'><img width='40px' height='40px' src='".asset('vendor/crudbooster/avatar.jpg')."'/></a>";
-            } else {
-                $pic = (strpos($value, 'http://') !== false) ? $value : asset($value);
-                $value = "<a data-lightbox='roadtrip'  rel='group_{{$table}}' title='$label: $title' href='".$pic."'><img width='40px' height='40px' src='".$pic."'/></a>";
-            }
-        }
+        list($col, $value) = $this->image($col, $table, $value, $label, $title);
 
-        if (@$col['download']) {
-            $url = (strpos($value, 'http://')) ? $value : asset($value).'?download=1';
-            if ($value) {
-                $value = "<a class='btn btn-xs btn-primary' href='$url' target='_blank' title='Download File'><i class='fa fa-download'></i> Download</a>";
-            } else {
-                $value = " - ";
-            }
-        }
+        list($col, $value) = $this->download($col, $value);
 
-        if ($col['str_limit']) {
-            $value = trim(strip_tags($value));
-            $value = str_limit($value, $col['str_limit']);
-        }
+        $value = $this->str_limit($col, $value);
 
         if ($col['nl2br']) {
             $value = nl2br($value);
@@ -58,6 +41,62 @@ class ValueCalculator
             if (count($prevalue)) {
                 $value = implode(", ", $prevalue);
             }
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param $col
+     * @param $table
+     * @param $value
+     * @param $label
+     * @param $title
+     * @return array
+     */
+    private function image($col, $table, $value, $label, $title)
+    {
+        if (isset($col['image'])) {
+            if ($value == '') {
+                $value = "<a  data-lightbox='roadtrip' rel='group_{{$table}}' title='$label: $title' href='".asset('vendor/crudbooster/avatar.jpg')."'><img width='40px' height='40px' src='".asset('vendor/crudbooster/avatar.jpg')."'/></a>";
+            } else {
+                $pic = (strpos($value, 'http://') !== false) ? $value : asset($value);
+                $value = "<a data-lightbox='roadtrip'  rel='group_{{$table}}' title='$label: $title' href='".$pic."'><img width='40px' height='40px' src='".$pic."'/></a>";
+            }
+        }
+
+        return [$col, $value];
+    }
+
+    /**
+     * @param $col
+     * @param $value
+     * @return array
+     */
+    private function download($col, $value)
+    {
+        if (isset($col['download'])) {
+            $url = (strpos($value, 'http://')) ? $value : asset($value).'?download=1';
+            if ($value) {
+                $value = "<a class='btn btn-xs btn-primary' href='$url' target='_blank' title='Download File'><i class='fa fa-download'></i> Download</a>";
+            } else {
+                $value = " - ";
+            }
+        }
+
+        return [$col, $value];
+    }
+
+    /**
+     * @param $col
+     * @param $value
+     * @return string
+     */
+    private function str_limit($col, $value)
+    {
+        if ($col['str_limit']) {
+            $value = trim(strip_tags($value));
+            $value = str_limit($value, $col['str_limit']);
         }
 
         return $value;
