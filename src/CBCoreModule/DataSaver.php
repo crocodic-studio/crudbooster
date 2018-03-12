96c526e2c332bcc9a3fcf88d2cbef2e10d2471bd
imanghafoori <imanghafoori1@gmail.com>
2018-03-12 19:42:54 +0330

    rename var

--- src/CBCoreModule/DataSaver.php
+++ src/CBCoreModule/DataSaver.php
@@ -84,13 +84,13 @@ class DataSaver
         $countInput = count(request($name.'-'.$columns[0]['name'])) - 1;
         $childArray = [];
         for ($i = 0; $i <= $countInput; $i++) {
-            $column_data = [];
-            $column_data[$fk] = $id;
+            $columnData = [];
+            $columnData[$fk] = $id;
             foreach ($columns as $col) {
                 $colName = $col['name'];
-                $column_data[$colName] = request($name.'-'.$colName)[$i];
+                $columnData[$colName] = request($name.'-'.$colName)[$i];
             }
-            $childArray[] = $column_data;
+            $childArray[] = $columnData;
         }
 
         $childTable = CRUDBooster::parseSqlTable($row['table'])['table'];
