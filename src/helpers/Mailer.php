1072c555f04be33ac5cf08cbeac31084b8b8dae9
imanghafoori <imanghafoori1@gmail.com>
2018-03-14 22:10:13 +0330

    private methods

--- src/helpers/Mailer.php
+++ src/helpers/Mailer.php
@@ -4,6 +4,8 @@ namespace crocodicstudio\crudbooster\helpers;
 
 class Mailer
 {
+    private $attachments;
+
     public function send($config)
     {
         $this->setConfigs();
@@ -19,25 +21,61 @@ class Mailer
             $template->subject = str_replace('['.$key.']', $val, $template->subject);
         }
         $subject = $template->subject;
-        $attachments = ($config['attachments']) ?: [];
+        $this->attachments = ($config['attachments']) ?: [];
 
         if ($config['send_at'] != null) {
-            $queue = [
-                'send_at' => $config['send_at'],
-                'email_recipient' => $to,
-                'email_from_email' => $template->from_email ?: SettingRepo::getSetting('email_sender'),
-                'email_from_name' => $template->from_name ?: SettingRepo::getSetting('appname'),
-                'email_cc_email' => $template->cc_email,
-                'email_subject' => $subject,
-                'email_content' => $html,
-                'email_attachments' => serialize($attachments),
-                'is_sent' => 0,
-            ];
-            DB::table('cms_email_queues')->insert($queue);
-
-            return true;
+            return $this->putInQueue($config, $template, $subject, $html);
         }
 
+        $this->sendMail($html, $to, $subject, $template);
+    }
+
+    private function setConfigs()
+    {
+        Config::set('mail.driver', SettingRepo::getSetting('smtp_driver'));
+        Config::set('mail.host', SettingRepo::getSetting('smtp_host'));
+        Config::set('mail.port', SettingRepo::getSetting('smtp_port'));
+        Config::set('mail.username', SettingRepo::getSetting('smtp_username'));
+        Config::set('mail.password', SettingRepo::getSetting('smtp_password'));
+    }
+
+    /**
+     * @param $config
+     * @param $to
+     * @param $template
+     * @param $subject
+     * @param $html
+     * @param $attachments
+     * @return bool
+     */
+    private function putInQueue($config, $template, $subject, $html, $attachments)
+    {
+        $to = $config['to'];
+        $queue = [
+            'send_at' => $config['send_at'],
+            'email_recipient' => $to,
+            'email_from_email' => $template->from_email ?: SettingRepo::getSetting('email_sender'),
+            'email_from_name' => $template->from_name ?: SettingRepo::getSetting('appname'),
+            'email_cc_email' => $template->cc_email,
+            'email_subject' => $subject,
+            'email_content' => $html,
+            'email_attachments' => serialize($attachments),
+            'is_sent' => 0,
+        ];
+        DB::table('cms_email_queues')->insert($queue);
+
+        return true;
+    }
+
+    /**
+     * @param $html
+     * @param $to
+     * @param $subject
+     * @param $template
+     * @param $attachments
+     */
+    private function sendMail($html, $to, $subject, $template, $attachments)
+    {
         \Mail::send("crudbooster::emails.blank", ['content' => $html], function ($message) use ($to, $subject, $template, $attachments) {
             $message->priority(1);
             $message->to($to);
@@ -59,14 +97,5 @@ class Mailer
 
             $message->subject($subject);
         });
-    }
-
-    private function setConfigs()
-    {
-        Config::set('mail.driver', SettingRepo::getSetting('smtp_driver'));
-        Config::set('mail.host', SettingRepo::getSetting('smtp_host'));
-        Config::set('mail.port', SettingRepo::getSetting('smtp_port'));
-        Config::set('mail.username', SettingRepo::getSetting('smtp_username'));
-        Config::set('mail.password', SettingRepo::getSetting('smtp_password'));
     }
 }
