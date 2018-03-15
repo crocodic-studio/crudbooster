81d6ddf64c8cb0a1a25e4a332da6335b20fc3cb9
imanghafoori <imanghafoori1@gmail.com>
2018-03-15 21:19:19 +0330

    extract into classes

--- src/helpers/CRUDBooster.php
+++ src/helpers/CRUDBooster.php
@@ -154,34 +154,17 @@ class CRUDBooster
 
     public static function getCurrentModule()
     {
-        $modulepath = self::getModulePath();
-        if (Cache::has('moduls_'.$modulepath)) {
-            return Cache::get('moduls_'.$modulepath);
-        }
-
-        return DB::table('cms_moduls')->where('path', self::getModulePath())->first();
+        return GetCurrentX::getCurrentModule();
     }
 
     public static function getCurrentDashboardId()
     {
-        if (request('d') == null) {
-            return session('currentDashboardId');
-        }
-        Session::put('currentDashboardId', request('d'));
-        Session::put('currentMenuId', 0);
-
-        return request('d');
+        return GetCurrentX::getCurrentDashboardId();
     }
 
     public static function getCurrentMenuId()
     {
-        if (request('m') == null) {
-            return session('currentMenuId');
-        }
-        Session::put('currentMenuId', request('m'));
-        Session::put('currentDashboardId', 0);
-
-        return request('m');
+        return GetCurrentX::getCurrentMenuId();
     }
 
     public static function myPrivilegeId()
@@ -215,21 +198,12 @@ class CRUDBooster
 
     public static function getCurrentId()
     {
-        $id = session('current_row_id');
-        $id = intval($id);
-        $id = (! $id) ? Request::segment(4) : $id;
-        $id = intval($id);
-
-        return $id;
+        return GetCurrentX::getCurrentId();
     }
 
     public static function getCurrentMethod()
     {
-        $action = str_replace(ctrlNamespace(), "", Route::currentRouteAction());
-        $atloc = strpos($action, '@') + 1;
-        $method = substr($action, $atloc);
-
-        return $method;
+        return GetCurrentX::getCurrentMethod();
     }
 
     public static function clearCache($name)
