96b2afd32bc678021a2715aafd231c3503791a2e
imanghafoori <imanghafoori1@gmail.com>
2018-03-21 20:43:07 +0330

    clean up

--- src/views/default/_form_body/component_assets.blade.php
+++ src/views/default/_form_body/component_assets.blade.php
@@ -19,9 +19,9 @@
 @endphp
 
 @foreach($types as $type)
-    @if(file_exists(CB::componentsPath($type).'/asset.blade.php'))
+    @if(file_exists(CB::componentsPath($type.'/asset.blade.php')))
         @include('crudbooster::default.type_components.'.$type.'.asset')
-    @elseif(file_exists(CB::PublishedComponentsPath($type).'/asset.blade.php')))
+    @elseif(file_exists(CB::PublishedComponentsPath($type.'/asset.blade.php')))
         @include('vendor.crudbooster.type_components.'.$type.'.asset')
     @endif
 @endforeach
