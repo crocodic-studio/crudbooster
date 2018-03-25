b727726afcefe838ead9030d2b93e8a81b69a106
imanghafoori <imanghafoori1@gmail.com>
2018-03-25 18:30:27 +0430

    extract trait

--- src/controllers/CBController.php
+++ src/controllers/CBController.php
@@ -12,6 +12,7 @@ use crocodicstudio\crudbooster\controllers\CBController\CbFormLoader;
 use crocodicstudio\crudbooster\controllers\CBController\CbIndexLoader;
 use crocodicstudio\crudbooster\controllers\CBController\CbLayoutLoader;
 use crocodicstudio\crudbooster\controllers\CBController\Deleter;
+use crocodicstudio\crudbooster\controllers\CBController\IndexAjax;
 use crocodicstudio\crudbooster\controllers\Helpers\IndexExport;
 use crocodicstudio\crudbooster\controllers\Helpers\IndexImport;
 use Illuminate\Support\Facades\Request;
@@ -27,7 +28,7 @@ use Schema;
 class CBController extends Controller
 {
     use Hooks;
-    use Deleter,CbFormLoader, CbIndexLoader, CbLayoutLoader;
+    use Deleter, CbFormLoader, CbIndexLoader, CbLayoutLoader, IndexAjax;
 
     public $data_inputan;
 
@@ -152,89 +153,6 @@ class CBController extends Controller
         $data = $index->index($this);
 
         return view('crudbooster::default.index', $data);
-    }
-
-    public function getDataQuery()
-    {
-        $key = request('query');
-        if (! Cache::has($key)) {
-            return response()->json(['items' => []]);
-        }
-        $query = Cache::get($key);
-
-        $fk_name = request('fk_name');
-        $fk_value = request('fk_value');
-
-        $condition = ' where ';
-        if (strpos(strtolower($query), 'where') !== false) {
-            $condition = ' and ';
-        }
-
-        if (strpos(strtolower($query), 'order by')) {
-            $query = str_replace('ORDER BY', 'order by', $query);
-            $qraw = explode('order by', $query);
-            $query = $qraw[0].$condition.$fk_name." = '$fk_value' $qraw[1]";
-        } else {
-            $query .= $condition.$fk_name." = '$fk_value'";
-        }
-
-        $query = DB::select(DB::raw($query));
-
-        return response()->json(['items' => $query]);
-    }
-
-    public function getDataTable()
-    {
-        $table = request('table');
-        $label = request('label');
-        $datatableWhere = urldecode(request('datatable_where'));
-        $foreign_key_name = request('fk_name');
-        $foreign_key_value = request('fk_value');
-        if (! $table || ! $label || ! $foreign_key_name || ! $foreign_key_value) {
-            return response()->json([]);
-        }
-        $query = DB::table($table);
-        if ($datatableWhere) {
-            $query->whereRaw($datatableWhere);
-        }
-        $query->select('id as select_value', $label.' as select_label');
-        $query->where($foreign_key_name, $foreign_key_value);
-        $query->orderby($label, 'asc');
-
-        return response()->json($query->get());
-    }
-
-    public function getDataModalDatatable()
-    {
-        $data = base64_decode(json_decode(request('data'), true));
-
-        $columns = explode(',', $data['columns']);
-
-        $result = DB::table($data['table']);
-        if (request('q')) {
-            $result->where(function ($where) use ($columns) {
-                foreach ($columns as $c => $col) {
-                    if ($c == 0) {
-                        $where->where($col, 'like', '%'.request('q').'%');
-                    } else {
-                        $where->orWhere($col, 'like', '%'.request('q').'%');
-                    }
-                }
-            });
-        }
-
-        if ($data['sql_where']) {
-            $result->whereraw($data['sql_where']);
-        }
-
-        if ($data['sql_orderby']) {
-            $result->orderByRaw($data['sql_orderby']);
-        } else {
-            $result->orderBy($data['column_value'], 'desc');
-        }
-        $limit = ($data['limit']) ?: 6;
-
-        return view('crudbooster::default.type_components.datamodal.browser', ['result' => $result->paginate($limit), 'data' => $data]);
     }
 
     public function getUpdateSingle()
