0e130de027f22835a62b6e0540a3aac83ae0b2ed
imanghafoori <imanghafoori1@gmail.com>
2018-03-29 17:09:45 +0430

    clean up and refactor

--- src/helpers/CRUDBooster.php
+++ src/helpers/CRUDBooster.php
@@ -3,6 +3,7 @@
 namespace crocodicstudio\crudbooster\helpers;
 
 use crocodicstudio\crudbooster\CBCoreModule\CbUsersRepo;
+use crocodicstudio\crudbooster\CBCoreModule\RouteController;
 use crocodicstudio\crudbooster\Modules\LogsModule\LogsRepository;
 use crocodicstudio\crudbooster\Modules\PrivilegeModule\PrivilegeHelpers;
 use Session;
@@ -363,39 +364,7 @@ class CRUDBooster
 
     public static function routeController($prefix, $controller, $namespace = null)
     {
-        $prefix = trim($prefix, '/').'/';
-
-        $namespace = ($namespace) ?: ctrlNamespace();
-
-        try {
-            Route::get($prefix, ['uses' => $controller.'@getIndex', 'as' => $controller.'GetIndex']);
-
-            $controller_class = new \ReflectionClass($namespace.'\\'.$controller);
-            $controller_methods = $controller_class->getMethods(\ReflectionMethod::IS_PUBLIC);
-            $wildcards = '/{one?}/{two?}/{three?}/{four?}/{five?}';
-            foreach ($controller_methods as $method) {
-
-                if ($method->class == 'Illuminate\Routing\Controller' || $method->name == 'getIndex') {
-                    continue;
-                }
-                if (substr($method->name, 0, 3) == 'get') {
-                    $method_name = substr($method->name, 3);
-                    $slug = array_filter(preg_split('/(?=[A-Z])/', $method_name));
-                    $slug = strtolower(implode('-', $slug));
-                    $slug = ($slug == 'index') ? '' : $slug;
-                    Route::get($prefix.$slug.$wildcards, ['uses' => $controller.'@'.$method->name, 'as' => $controller.'Get'.$method_name]);
-                } elseif (substr($method->name, 0, 4) == 'post') {
-                    $method_name = substr($method->name, 4);
-                    $slug = array_filter(preg_split('/(?=[A-Z])/', $method_name));
-                    Route::post($prefix.strtolower(implode('-', $slug)).$wildcards, [
-                            'uses' => $controller.'@'.$method->name,
-                            'as' => $controller.'Post'.$method_name,
-                        ]);
-                }
-            }
-        } catch (\Exception $e) {
-
-        }
+        RouteController::routeController($prefix, $controller, $namespace);
     }
 
     /*
