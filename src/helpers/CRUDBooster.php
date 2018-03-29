1ba3568dbefa2d367be454c6809e96db07fac418
imanghafoori <imanghafoori1@gmail.com>
2018-03-29 17:56:51 +0430

    extract Class

--- src/helpers/CRUDBooster.php
+++ src/helpers/CRUDBooster.php
@@ -218,28 +218,8 @@ class CRUDBooster
 
     public static function urlFilterColumn($key, $type, $value = '', $singleSorting = true)
     {
-        $params = request()->all();
-        $mainpath = trim(self::mainpath(), '/');
-
-        if ($params['filter_column'] && $singleSorting) {
-            foreach ($params['filter_column'] as $k => $filter) {
-                foreach ($filter as $t => $val) {
-                    if ($t == 'sorting') {
-                        unset($params['filter_column'][$k]['sorting']);
-                    }
-                }
-            }
-        }
-
-        $params['filter_column'][$key][$type] = $value;
-
-        if (isset($params)) {
-            return $mainpath.'?'.http_build_query($params);
-        }
-        return $mainpath.'?filter_column['.$key.']['.$type.']='.$value;
-
+        \crocodicstudio\crudbooster\CBCoreModule\Index\ViewHelpers::urlFilterColumn($key, $type, $value, $singleSorting);
     }
-
     public static function mainpath($path = null)
     {
         $controllerName = strtok(Route::currentRouteAction(), '@');
