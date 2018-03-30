1a5872409934b48c0085e5142a9b5bb3d49377ed
imanghafoori <imanghafoori1@gmail.com>
2018-03-30 18:56:04 +0430

    refactor

--- src/Modules/ModuleGenerator/ControllerGenerator/FieldDetector.php
+++ src/Modules/ModuleGenerator/ControllerGenerator/FieldDetector.php
@@ -8,27 +8,55 @@ class FieldDetector
      * @param $fieldName string
      * @return bool
      */
+    static function isExceptional($fieldName)
+    {
+        return in_array($fieldName, ['id', 'created_at', 'updated_at', 'deleted_at']);
+    }
+
+    /**
+     * @param $fieldName string
+     * @return bool
+     */
+    static function isForeignKey($fieldName)
+    {
+        return substr($fieldName, 0, 3) == 'id_' || substr($fieldName, -3) == '_id';
+    }
+
+    static function isUploadField($fieldName)
+    {
+        return self::isWithin($fieldName, 'UPLOAD_TYPES');
+    }
+
+    static function isWithin($fieldName, $configKey)
+    {
+        return in_array($fieldName, explode(',', cbConfig($configKey)));
+    }
+
+    static function detect($colName)
+    {
+        $map = [
+            'isPassword' => 'password',
+            'isImage' => 'image',
+            'isGeographical' => 'geo',
+            'isPhone' => 'phone',
+            'isEmail' => 'email',
+            'isNameField' => 'name',
+            'isUrlField' => 'url',
+        ];
+        foreach ($map as $methodName => $fieldType){
+            if (self::$methodName($colName)) {
+                return $fieldType;
+            }
+        }
+    }
+
+    /**
+     * @param $fieldName string
+     * @return bool
+     */
     static function isPassword($fieldName)
     {
         return self::isWithin($fieldName, 'PASSWORD_FIELDS_CANDIDATE');
-    }
-
-    /**
-     * @param $fieldName string
-     * @return bool
-     */
-    static function isEmail($fieldName)
-    {
-        return self::isWithin($fieldName, 'EMAIL_FIELDS_CANDIDATE');
-    }
-
-    /**
-     * @param $fieldName string
-     * @return bool
-     */
-    static function isPhone($fieldName)
-    {
-        return self::isWithin($fieldName, 'PHONE_FIELDS_CANDIDATE');
     }
 
     /**
@@ -44,27 +72,27 @@ class FieldDetector
      * @param $fieldName string
      * @return bool
      */
-    static function isExceptional($fieldName)
-    {
-        return in_array($fieldName, ['id', 'created_at', 'updated_at', 'deleted_at']);
-    }
-
-    /**
-     * @param $fieldName string
-     * @return bool
-     */
-    static function isForeignKey($fieldName)
-    {
-        return substr($fieldName, 0, 3) == 'id_' || substr($fieldName, -3) == '_id';
-    }
-
-    /**
-     * @param $fieldName string
-     * @return bool
-     */
     static function isGeographical($fieldName)
     {
         return in_array($fieldName, ['latitude', 'longitude']);
+    }
+
+    /**
+     * @param $fieldName string
+     * @return bool
+     */
+    static function isPhone($fieldName)
+    {
+        return self::isWithin($fieldName, 'PHONE_FIELDS_CANDIDATE');
+    }
+
+    /**
+     * @param $fieldName string
+     * @return bool
+     */
+    static function isEmail($fieldName)
+    {
+        return self::isWithin($fieldName, 'EMAIL_FIELDS_CANDIDATE');
     }
 
     /**
@@ -83,15 +111,5 @@ class FieldDetector
     static function isUrlField($fieldName)
     {
         return self::isWithin($fieldName, 'URL_FIELDS_CANDIDATE');
-    }
-
-    static function isUploadField($fieldName)
-    {
-        return self::isWithin($fieldName, 'UPLOAD_TYPES');
-    }
-
-    static function isWithin($fieldName, $configKey)
-    {
-        return in_array($fieldName, explode(',', cbConfig($configKey)));
     }
 }
