0aa865b3278fe479f8e65367f3f73b620f4bb376
imanghafoori <imanghafoori1@gmail.com>
2018-03-31 17:51:18 +0430

    rename vars

--- src/controllers/CBController/CbIndexLoader.php
+++ src/controllers/CBController/CbIndexLoader.php
@@ -4,59 +4,59 @@ namespace crocodicstudio\crudbooster\controllers\CBController;
 
 trait CbIndexLoader
 {
-    public $password_candidate = null;
+    //public $password_candidate = null;
 
-    public $date_candidate = null;
+    //public $date_candidate = null;
 
     public $columns_table;
 
     public $orderby = null;
 
-    public $index_statistic = [];
+    public $indexStatistic = [];
 
-    public $index_additional_view = [];
+    public $indexAdditionalView = [];
 
-    public $pre_index_html = null;
+    public $preIndexHtml = null;
 
-    public $post_index_html = null;
+    public $postIndexHtml = null;
 
-    public $show_numbering = false;
+    public $showNumbering = false;
 
     public $index_button = [];
 
     public $limit = 20;
 
-    public $table_row_color = [];
+    public $tableRowColor = [];
 
-    public $button_table_action = true;
+    public $buttonTableAction = true;
 
-    public $button_bulk_action = true;
+    public $buttonBulkAction = true;
 
-    public $button_filter = true;
+    public $buttonFilter = true;
 
-    public $button_show = true;
+    public $buttonShow = true;
 
     public $sub_module = [];
 
-    public $button_action_width = null;
+    public $buttonActionWidth = null;
 
     public $addaction = [];
 
     protected function cbIndexLoader()
     {
-        $this->data['button_export'] = $this->button_export;
-        $this->data['button_import'] = $this->button_import;
-        $this->data['button_filter'] = $this->button_filter;
-        $this->data['show_numbering'] = $this->show_numbering;
-        $this->data['pre_index_html'] = $this->pre_index_html;
-        $this->data['post_index_html'] = $this->post_index_html;
-        $this->data['index_statistic'] = $this->index_statistic;
-        $this->data['table_row_color'] = $this->table_row_color;
-        $this->data['button_bulk_action'] = $this->button_bulk_action;
-        $this->data['button_table_action'] = $this->button_table_action;
-        $this->data['index_additional_view'] = $this->index_additional_view;
-        $this->data['button_action_width'] = $this->button_action_width;
-        $this->data['button_show'] = $this->button_show;
+        $this->data['button_export'] = $this->buttonExport;
+        $this->data['button_import'] = $this->buttonImport;
+        $this->data['button_filter'] = $this->buttonFilter;
+        $this->data['show_numbering'] = $this->showNumbering;
+        $this->data['pre_index_html'] = $this->preIndexHtml;
+        $this->data['post_index_html'] = $this->postIndexHtml;
+        $this->data['index_statistic'] = $this->indexStatistic;
+        $this->data['table_row_color'] = $this->tableRowColor;
+        $this->data['button_bulk_action'] = $this->buttonBulkAction;
+        $this->data['button_table_action'] = $this->buttonTableAction;
+        $this->data['index_additional_view'] = $this->indexAdditionalView;
+        $this->data['button_action_width'] = $this->buttonActionWidth;
+        $this->data['button_show'] = $this->buttonShow;
         $this->data['addaction'] = ($this->show_addaction) ? $this->addaction : null;
     }
 
