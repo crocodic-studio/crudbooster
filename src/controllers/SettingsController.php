da938eec62e8b1ff37e408142512c60ee47fecf6
Adrian Lim <adrian.lbs@gmail.com>
2018-04-09 22:10:25 +0800

    Change request use

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -3,7 +3,7 @@
 use crocodicstudio\crudbooster\controllers\Controller;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Request;
+//use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Mail;
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\PDF;
 use Illuminate\Support\Facades\Excel;
+use Illuminate\Http\Request;
 use CRUDBooster;
 
 class SettingsController extends CBController {
