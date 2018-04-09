16effabf734786fbdec77c1ad00406fc3c6c95a8
imanghafoori <imanghafoori1@gmail.com>
2018-04-09 22:38:44 +0430

    refactor

--- src/Modules/ModuleGenerator/ControllerGenerator.php
+++ src/Modules/ModuleGenerator/ControllerGenerator.php
@@ -9,7 +9,8 @@ use Schema;
 
 class ControllerGenerator
 {
-    public static function generateController($table, $name = null){
+    public static function generateController($table, $name = null)
+    {
 
         $controllerName = self::getControllerName($table, $name);
 
@@ -19,6 +20,7 @@ class ControllerGenerator
 
         return 'Admin'.$controllerName;
     }
+
     /**
      * @param $table
      * @param $name
@@ -46,54 +48,6 @@ class ControllerGenerator
 
     /**
      * @param $table
-     * @param $coloms
-     * @param $pk
-     * @return array
-     */
-    private static function addCol($table, $coloms, $pk)
-    {
-        $coloms_col = array_slice($coloms, 0, 8);
-        $joinList = [];
-        $cols = [];
-
-        foreach ($coloms_col as $c) {
-            $label = str_replace("id_", "", $c);
-            $label = ucwords(str_replace("_", " ", $label));
-            $label = str_replace('Cms ', '', $label);
-            $field = $c;
-
-            if (FieldDetector::isExceptional($field) || FieldDetector::isPassword($field)) {
-                continue;
-            }
-
-            if (FieldDetector::isForeignKey($field)) {
-                $jointable = str_replace(['id_', '_id'], '', $field);
-
-                if (Schema::hasTable($jointable)) {
-                    $joincols = \Schema::getColumnListing($jointable);
-                    $joinname = DbInspector::colName($joincols);
-                    $cols[] = ['label' => $label, 'name' =>  $jointable.$joinname];
-                    $jointablePK = DbInspector::findPk($jointable);
-                    $joinList[] = [
-                        'table' => $jointable,
-                        'field1' => $jointable.'.'.$jointablePK,
-                        'field2' => $table.'.'.$pk,
-                    ];
-                }
-            } else {
-                $image = '';
-                if (FieldDetector::isImage($field)) {
-                    $image = '"image" => true';
-                }
-                $cols[] = ['label' => $label, 'name' =>  "'$field', $image"];
-            }
-        }
-
-        return [$cols, $joinList];
-    }
-
-    /**
-     * @param $table
      * @param $controllerName
      * @return string
      * @throws \Exception
@@ -107,6 +61,84 @@ class ControllerGenerator
         list($cols, $joinList) = self::addCol($table, $coloms, $pk);
 
         $data = compact('controllerName', 'table', 'pk', 'coloms', 'cols', 'formArrayString', 'joinList');
+
         return '<?php '.view('CbModulesGen::controller_stub', $data)->render();
+    }
+
+    /**
+     * @param $table
+     * @param $coloms
+     * @param $pk
+     * @return array
+     */
+    private static function addCol($table, $coloms, $pk)
+    {
+        $coloms_col = array_slice($coloms, 0, 8);
+        $joinList = [];
+        $cols = [];
+        array_filter($coloms_col, function ($field) {
+            return (! FieldDetector::isExceptional($field) && ! FieldDetector::isPassword($field));
+        });
+
+        foreach ($coloms_col as $field) {
+            $label = str_replace("id_", "", $field);
+            $label = ucwords(str_replace("_", " ", $label));
+            $label = str_replace('Cms ', '', $label);
+
+            if (FieldDetector::isForeignKey($field)) {
+                list($cols, $joinList) = self::handleForeignKey($table, $pk, $field, $label, $cols, $joinList);
+            } else {
+                $cols = self::handleOtherFields($field, $label, $cols);
+            }
+        }
+
+        return [$cols, $joinList];
+    }
+
+    /**
+     * @param $table
+     * @param $pk
+     * @param $field
+     * @param $label
+     * @param $cols
+     * @param $joinList
+     * @return array
+     * @throws \Exception
+     */
+    private static function handleForeignKey($table, $pk, $field, $label, $cols, $joinList)
+    {
+        $jointable = str_replace(['id_', '_id'], '', $field);
+
+        if (! Schema::hasTable($jointable)) {
+            return [$cols, $joinList];
+        }
+        $joincols = \Schema::getColumnListing($jointable);
+        $joinname = DbInspector::colName($joincols);
+        $cols[] = ['label' => $label, 'name' => $jointable.$joinname];
+        $jointablePK = DbInspector::findPk($jointable);
+        $joinList[] = [
+            'table' => $jointable,
+            'field1' => $jointable.'.'.$jointablePK,
+            'field2' => $table.'.'.$pk,
+        ];
+
+        return [$cols, $joinList];
+    }
+
+    /**
+     * @param $field
+     * @param $label
+     * @param $cols
+     * @return array
+     */
+    private static function handleOtherFields($field, $label, $cols)
+    {
+        $image = '';
+        if (FieldDetector::isImage($field)) {
+            $image = '"image" => true';
+        }
+        $cols[] = ['label' => $label, 'name' => "'$field', $image"];
+
+        return $cols;
     }
 }
