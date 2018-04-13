9f14e33b888a54fe1519626171ca8d4fc009ce6c
imanghafoori <imanghafoori1@gmail.com>
2018-04-13 13:28:09 +0430

    Debug for php 7.2.1

--- src/views/default/type_components/number/component.blade.php
+++ src/views/default/type_components/number/component.blade.php
@@ -2,7 +2,7 @@
 							<label class='control-label col-sm-2'>{{$form['label']}} {!!($required)?"<span class='text-danger' title='This field is required'>*</span>":"" !!}</label>
 
 							<div class="{{$col_width?:'col-sm-10'}}">
-							<input type='number' step="{{($form['step'])?:'1'}}" title="{{$form['label']}}" {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} {{$validation['min']?"min=$validation[min]":""}} {{$validation['max']?"max=$validation[max]":""}} class='form-control' name="{{$name}}" id="{{$name}}" value='{{$value}}'/>
+							<input type='number' step="{{($form['step'])?:'1'}}" title="{{$form['label']}}" {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} {{$validation['min']?"min=".$validation['min']:""}} {{$validation['max']?"max=".$validation['max']:""}} class='form-control' name="{{$name}}" id="{{$name}}" value='{{$value}}'/>
 							<div class="text-danger">{!! $errors->first($name)?"<i class='fa fa-info-circle'></i> ".$errors->first($name):"" !!}</div>
 							<p class='help-block'>{{ @$form['help'] }}</p>
 							</div>
