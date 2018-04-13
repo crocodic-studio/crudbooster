b19524c55b25302744dbab145bbd63be0bf00904
imanghafoori <imanghafoori1@gmail.com>
2018-04-13 22:38:43 +0430

    update version

--- src/views/admin_template.blade.php
+++ src/views/admin_template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>{{ ($page_title)?Session::get('appname').': '.strip_tags($page_title):"Admin Area" }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <meta name='generator' content='CRUDBooster 5.4.0.1'/>
+    <meta name='generator' content='CRUDBooster 5.4.6'/>
     <meta name='robots' content='noindex,nofollow'/>
     <link rel="shortcut icon"
           href="{{ CRUDBooster::getSetting('favicon')?asset(CRUDBooster::getSetting('favicon')):asset('vendor/crudbooster/assets/logo_crudbooster.png') }}">
