de20e49ef63c2a87a6003dd39633d5838e4a0d3f
imanghafoori <imanghafoori1@gmail.com>
2018-04-13 22:19:07 +0430

    bug fix

--- src/views/admin_template.blade.php
+++ src/views/admin_template.blade.php
@@ -141,8 +141,8 @@
                         @foreach($index_button as $ib)
                             <a href='{{$ib["url"]}}' id='{{str_slug($ib["label"])}}' class='btn {{($ib['color'])?'btn-'.$ib['color']:'btn-primary'}} btn-sm'
                                @if($ib['onClick']) onClick='return {{$ib["onClick"]}}' @endif
-                               @if($ib['onMouseOever']) onMouseOever='return {{$ib["onMouseOever"]}}' @endif
-                               @if($ib['onMoueseOut']) onMoueseOut='return {{$ib["onMoueseOut"]}}' @endif
+                               @if($ib['onMouseOver']) onMouseOver='return {{$ib["onMouseOver"]}}' @endif
+                               @if($ib['onMouseOut']) onMouseOut='return {{$ib["onMouseOut"]}}' @endif
                                @if($ib['onKeyDown']) onKeyDown='return {{$ib["onKeyDown"]}}' @endif
                                @if($ib['onLoad']) onLoad='return {{$ib["onLoad"]}}' @endif
                             >
