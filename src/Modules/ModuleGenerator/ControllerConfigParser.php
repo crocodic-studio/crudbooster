2f940054c09083b50c87912b05f487b43d240b42
imanghafoori <imanghafoori1@gmail.com>
2018-04-14 12:07:38 +0430

    clean up

--- src/Modules/ModuleGenerator/ControllerConfigParser.php
+++ src/Modules/ModuleGenerator/ControllerConfigParser.php
@@ -4,13 +4,25 @@ namespace crocodicstudio\crudbooster\Modules\ModuleGenerator;
 
 class ControllerConfigParser
 {
-    static function parse($code)
+    public static function parse($code)
     {
         $configCode = ScaffoldingParser::extract_unit($code, cbStartMarker('CONFIGURATION'), cbEndMarker('CONFIGURATION'));
         $configCode = preg_replace('/[\t\n\r\0\x0B]/', '', $configCode);
         $configCode = preg_replace('/([\s])\1+/', ' ', $configCode);
         $configCode = explode(";", $configCode);
         $configCode = array_map('trim', $configCode);
+        $result = self::toArray($code, $configCode);
+
+        return $result;
+    }
+
+    /**
+     * @param $code
+     * @param $configCode
+     * @return array
+     */
+    private static function toArray($code, $configCode)
+    {
         $result = [];
         foreach ($configCode as &$code) {
             $key = substr($code, 0, strpos($code, ' = '));
