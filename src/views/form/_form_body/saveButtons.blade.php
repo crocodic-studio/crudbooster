03ee32c2a707fb645f514dabec4242ae2e7212c8
imanghafoori <imanghafoori1@gmail.com>
2018-04-20 16:46:52 +0430

    bug fix

--- src/views/form/_form_body/saveButtons.blade.php
+++ src/views/form/_form_body/saveButtons.blade.php
@@ -1,12 +1,10 @@
 @if($button_cancel && CRUDBooster::getCurrentMethod() != 'getDetail')
-    <a href='
-@if(request('return_url'))
-    {{request("return_url")}}
-    @else
-    {{CRUDBooster::mainpath("?".http_build_query(@$_GET)) }}
-    @endif
-            ' class='btn btn-default'>
-        <i class='fa fa-chevron-circle-left'></i> {{ cbTrans("button_back")}}</a>
+    @php
+        $url = request('return_url') ? request("return_url"): CRUDBooster::mainpath("?".http_build_query(@$_GET));
+    @endphp
+    <a href='{!! $url !!}' class='btn btn-default'>
+        <i class='fa fa-chevron-circle-left'></i> {{ cbTrans("button_back")}}
+    </a>
 @endif
 
 
