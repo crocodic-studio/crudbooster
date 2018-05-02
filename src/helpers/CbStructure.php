d25eb32ae47000080b73c141f02002b8de8a7434
imanghafoori <imanghafoori1@gmail.com>
2018-05-02 16:12:39 +0430

    added php 7 type hints

--- src/helpers/CbStructure.php
+++ src/helpers/CbStructure.php
@@ -4,37 +4,38 @@ namespace crocodicstudio\crudbooster\helpers;
 
 class CbStructure
 {
-    public static function componentsPath($type = '')
+    public static function componentsPath(string $type = ''): string
     {
         $componentPath = implode(DIRECTORY_SEPARATOR, ['vendor', 'crocodicstudio', 'crudbooster', 'src', 'views', 'form', 'type_components', $type]);
 
         return base_path($componentPath);
     }
 
-    public static function publishedComponentsPath($type = '')
+    public static function publishedComponentsPath(string $type = ''): string
     {
         $Path = implode(DIRECTORY_SEPARATOR, ['views', 'vendor', 'crudbooster', 'type_components', $type]);
 
         return resource_path($Path);
     }
 
-    public static function controllersDir()
-    {
-        $_ = DIRECTORY_SEPARATOR;
-        return app_path('Http'.$_.'Controllers'.$_);
-    }
-
-    public static function controllerPath($controller)
+    public static function controllerPath(string $controller): string
     {
         return self::controllersDir().$controller.'.php';
     }
 
-    public static function cbModulesNS($path = '')
+    public static function controllersDir(): string
+    {
+        $_ = DIRECTORY_SEPARATOR;
+
+        return app_path('Http'.$_.'Controllers'.$_);
+    }
+
+    public static function cbModulesNS(string $path = ''): string
     {
         return '\crocodicstudio\crudbooster\Modules\\'.$path;
     }
 
-    public static function ctrlNamespace()
+    public static function ctrlNamespace(): string
     {
         return 'App\Http\Controllers';
     }
