69cd164fdb4e3a3c624df8e0fdf00ddd06a31ce6
imanghafoori <imanghafoori1@gmail.com>
2018-05-02 11:32:32 +0430

    clean up

--- src/Modules/FileManagerModule/file_manager_routes.php
+++ src/Modules/FileManagerModule/file_manager_routes.php
@@ -1,137 +1,36 @@
 <?php
 
-$namespace = cbModulesNS('FileManagerModule');
-
 Route::group([
     'middleware' => ['web', \crocodicstudio\crudbooster\middlewares\CBSuperadmin::class],
     'prefix' => cbAdminPath(),
-    'namespace' => $namespace,
-], function () use ($namespace) {
-    Route::get('file-manager/', ['uses' => 'AdminFileManagerController@getIndex', 'as' => 'AdminFileManagerControllerGetIndex']);
+    'namespace' => cbModulesNS('FileManagerModule'),
+], function (){
+    $ctrl = 'AdminFileManagerController';
+    Route::get('file-manager/', $ctrl.'@getIndex')->name($ctrl.'GetIndex');
 
-    Route::post('file-manager/create-directory/{one?}/{two?}/{three?}/{four?}/{five?}', [
-        'uses' => 'AdminFileManagerController@postCreateDirectory',
-        'as' => 'AdminFileManagerControllerPostCreateDirectory',
-    ]);
-
-    Route::post('file-manager/upload', [
-        'uses' => 'AdminFileManagerController@postUpload',
-        'as' => 'AdminFileManagerControllerPostUpload',
-    ]);
-
-    Route::get('file-manager/delete-directory/{one?}', [
-        'uses' => 'AdminFileManagerController@getDeleteDirectory',
-        'as' => 'AdminFileManagerControllerGetDeleteDirectory',
-    ]);
-
-    Route::get('file-manager/delete-file/{file}', [
-        'uses' => 'AdminFileManagerController@getDeleteFile',
-        'as' => 'AdminFileManagerControllerGetDeleteFile',
-    ]);
-
-    Route::get('file-manager/export-data', [
-        'uses' => 'AdminFileManagerController@getExportData',
-        'as' => 'AdminFileManagerControllerGetExportData',
-    ]);
-
-    Route::post('file-manager/export-data', [
-        'uses' => 'AdminFileManagerController@postExportData',
-        'as' => 'AdminFileManagerControllerPostExportData',
-    ]);
-
-    Route::get('file-manager/data-query', [
-        'uses' => 'AdminFileManagerController@getDataQuery',
-        'as' => 'AdminFileManagerControllerGetDataQuery',
-    ]);
-
-    Route::get('file-manager/data-table', [
-        'uses' => 'AdminFileManagerController@getDataTable',
-        'as' => 'AdminFileManagerControllerGetDataTable',
-    ]);
-
-    Route::get('file-manager/data-modal-datatable', [
-        'uses' => 'AdminFileManagerController@getDataModalDatatable',
-        'as' => 'AdminFileManagerControllerGetDataModalDatatable',
-    ]);
-
-    Route::get('file-manager/update-single', [
-        'uses' => 'AdminFileManagerController@getUpdateSingle',
-        'as' => 'AdminFileManagerControllerGetUpdateSingle',
-    ]);
-
-    Route::post('file-manager/find-data', [
-        'uses' => 'AdminFileManagerController@postFindData',
-        'as' => 'AdminFileManagerControllerPostFindData',
-    ]);
-
-    Route::get('file-manager/add', [
-        'uses' => 'AdminFileManagerController@getAdd',
-        'as' => 'AdminFileManagerControllerGetAdd',
-    ]);
-
-    Route::post('file-manager/add-save', [
-        'uses' => 'AdminFileManagerController@postAddSave',
-        'as' => 'AdminFileManagerControllerPostAddSave',
-    ]);
-
-    Route::get('file-manager/edit/{id?}', [
-        'uses' => 'AdminFileManagerController@getEdit',
-        'as' => 'AdminFileManagerControllerGetEdit',
-    ]);
-
-    Route::post('file-manager/edit-save/{id?}', [
-        'uses' => 'AdminFileManagerController@postEditSave',
-        'as' => 'AdminFileManagerControllerPostEditSave',
-    ]);
-
-    Route::get('file-manager/delete/{id?}', [
-        'uses' => 'AdminFileManagerController@getDelete',
-        'as' => 'AdminFileManagerControllerGetDelete',
-    ]);
-
-    Route::get('file-manager/detail/{id?}', [
-        'uses' => 'AdminFileManagerController@getDetail',
-        'as' => 'AdminFileManagerControllerGetDetail',
-    ]);
-
-    Route::get('file-manager/import-data', [
-        'uses' => 'AdminFileManagerController@getImportData',
-        'as' => 'AdminFileManagerControllerGetImportData',
-    ]);
-
-/*    Route::post('file-manager/done-import', [
-        'uses' => 'AdminFileManagerController@postDoneImport',
-        'as' => 'AdminFileManagerControllerPostDoneImport',
-    ]);*/
-
-    Route::post('file-manager/do-import-chunk', [
-        'uses' => 'AdminFileManagerController@postDoImportChunk',
-        'as' => 'AdminFileManagerControllerPostDoImportChunk',
-    ]);
-
- /*   Route::post('file-manager/do-upload-import-data', [
-        'uses' => 'AdminFileManagerController@postDoUploadImportData',
-        'as' => 'AdminFileManagerControllerPostDoUploadImportData',
-    ]);*/
-
-    Route::post('file-manager/action-selected', [
-        'uses' => 'AdminFileManagerController@postActionSelected',
-        'as' => 'AdminFileManagerControllerPostActionSelected',
-    ]);
-
-    Route::get('file-manager/delete-image', [
-        'uses' => 'AdminFileManagerController@getDeleteImage',
-        'as' => 'AdminFileManagerControllerGetDeleteImage',
-    ]);
-
-/*    Route::post('file-manager/upload-summernote', [
-        'uses' => 'AdminFileManagerController@postUploadSummernote',
-        'as' => 'AdminFileManagerControllerPostUploadSummernote',
-    ]);
-
-    Route::post('file-manager/upload-file', [
-        'uses' => 'AdminFileManagerController@postUploadFile',
-        'as' => 'AdminFileManagerControllerPostUploadFile',
-    ]);*/
-
+    Route::post('file-manager/create-directory/{one?}/{two?}/{three?}/{four?}/{five?}', $ctrl.'@postCreateDirectory')->name($ctrl.'PostCreateDirectory');
+    Route::post('file-manager/upload', $ctrl.'@postUpload')->name($ctrl.'PostUpload');
+    Route::get('file-manager/delete-directory/{one?}', $ctrl.'@getDeleteDirectory')->name($ctrl.'GetDeleteDirectory');
+    Route::get('file-manager/delete-file/{file}', $ctrl.'@getDeleteFile')->name($ctrl.'GetDeleteFile');
+    Route::get('file-manager/export-data', $ctrl.'@getExportData')->name($ctrl.'GetExportData');
+    Route::post('file-manager/export-data', $ctrl.'@postExportData')->name($ctrl.'PostExportData');
+    Route::get('file-manager/data-query', $ctrl.'@getDataQuery')->name($ctrl.'GetDataQuery');
+    Route::get('file-manager/data-table', $ctrl.'@getDataTable')->name($ctrl.'GetDataTable');
+    Route::get('file-manager/data-modal-datatable', $ctrl.'@getDataModalDatatable')->name($ctrl.'GetDataModalDatatable');
+    Route::get('file-manager/update-single', $ctrl.'@getUpdateSingle')->name($ctrl.'GetUpdateSingle');
+    Route::post('file-manager/find-data', $ctrl.'@postFindData')->name($ctrl.'PostFindData');
+    Route::get('file-manager/add', $ctrl.'@getAdd')->name($ctrl.'GetAdd');
+    Route::post('file-manager/add-save', $ctrl.'@postAddSave')->name($ctrl.'PostAddSave');
+    Route::get('file-manager/edit/{id?}', $ctrl.'@getEdit')->name($ctrl.'GetEdit');
+    Route::post('file-manager/edit-save/{id?}', $ctrl.'@postEditSave')->name($ctrl.'PostEditSave');
+    Route::get('file-manager/delete/{id?}', $ctrl.'@getDelete')->name($ctrl.'GetDelete');
+    Route::get('file-manager/detail/{id?}', $ctrl.'@getDetail')->name($ctrl.'GetDetail');
+    Route::get('file-manager/import-data', $ctrl.'@getImportData')->name($ctrl.'GetImportData');
+    Route::post('file-manager/done-import', $ctrl.'@postDoneImport')->name($ctrl.'PostDoneImport');
+    Route::post('file-manager/do-import-chunk', $ctrl.'@postDoImportChunk')->name($ctrl.'PostDoImportChunk');
+    Route::post('file-manager/do-upload-import-data', $ctrl.'@postDoUploadImportData')->name($ctrl.'PostDoUploadImportData');
+    Route::post('file-manager/action-selected', $ctrl.'@postActionSelected')->name($ctrl.'PostActionSelected');
+    Route::get('file-manager/delete-image', $ctrl.'@getDeleteImage')->name($ctrl.'GetDeleteImage');
+    //Route::post('file-manager/upload-summernote', 'AdminFileManagerController@postUploadSummernote')->name('AdminFileManagerControllerPostUploadSummernote');
+    //Route::post('file-manager/upload-file', 'AdminFileManagerController@postUploadFile')->name('AdminFileManagerControllerPostUploadFile');
 });
