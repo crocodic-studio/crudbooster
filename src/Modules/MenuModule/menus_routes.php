da01bedad7ec942d611eee792055c9fc45333553
imanghafoori <imanghafoori1@gmail.com>
2018-05-04 13:24:43 +0430

    clean up

--- src/Modules/MenuModule/menus_routes.php
+++ src/Modules/MenuModule/menus_routes.php
@@ -9,34 +9,27 @@ Route::group([
     'prefix' => cbAdminPath(),
     'namespace' => cbModulesNS('MenuModule'),
 ], function () {
-    Route::get('menus/', ['uses' => 'AdminMenusController@getIndex', 'as' => 'AdminMenusControllerGetIndex']);
-
-    Route::get('menus/export-data', ['uses' => 'AdminMenusController@getExportData', 'as' => 'AdminMenusControllerGetExportData']);
-    Route::post('menus/export-data', ['uses' => 'AdminMenusController@postExportData', 'as' => 'AdminMenusControllerPostExportData',]);
-    Route::get('menus/import-data', ['uses' => 'AdminMenusController@getImportData', 'as' => 'AdminMenusControllerGetImportData']);
-    //Route::post('menus/done-import', ['uses' => 'AdminMenusController@postDoneImport', 'as' => 'AdminMenusControllerPostDoneImport',]);
-    Route::post('menus/do-import-chunk', ['uses' => 'AdminMenusController@postDoImportChunk', 'as' => 'AdminMenusControllerPostDoImportChunk',]);
-
-    Route::get('menus/data-query', ['uses' => 'AdminMenusController@getDataQuery', 'as' => 'AdminMenusControllerGetDataQuery']);
-    Route::get('menus/data-table', ['uses' => 'AdminMenusController@getDataTable', 'as' => 'AdminMenusControllerGetDataTable']);
-    Route::get('menus/data-modal-datatable', ['uses' => 'AdminMenusController@getDataModalDatatable', 'as' => 'AdminMenusControllerGetDataModalDatatable']);
-    Route::get('menus/update-single', ['uses' => 'AdminMenusController@getUpdateSingle', 'as' => 'AdminMenusControllerGetUpdateSingle']);
-    Route::get('menus/add', ['uses' => 'AdminMenusController@getAdd', 'as' => 'AdminMenusControllerGetAdd']);
-    Route::get('menus/edit/{id?}', ['uses' => 'AdminMenusController@getEdit', 'as' => 'AdminMenusControllerGetEdit']);
-    Route::get('menus/delete/{id?}', ['uses' => 'AdminMenusController@getDelete', 'as' => 'AdminMenusControllerGetDelete']);
-    Route::get('menus/detail/{id?}', ['uses' => 'AdminMenusController@getDetail', 'as' => 'AdminMenusControllerGetDetail']);
-    Route::get('menus/delete-image', ['uses' => 'AdminMenusController@getDeleteImage', 'as' => 'AdminMenusControllerGetDeleteImage']);
-
-    Route::post('menus/save-menu', ['uses' => 'AdminMenusController@postSaveMenu', 'as' => 'AdminMenusControllerPostSaveMenu',]);
-
-    Route::post('menus/export-data', ['uses' => 'AdminMenusController@postExportData', 'as' => 'AdminMenusControllerPostExportData',]);
-
-    Route::post('menus/add-save', ['uses' => 'AdminMenusController@postAddSave', 'as' => 'AdminMenusControllerPostAddSave',]);
-    Route::post('menus/edit-save/{id?}', ['uses' => 'AdminMenusController@postEditSave', 'as' => 'AdminMenusControllerPostEditSave',]);
-    Route::post('menus/find-data', ['uses' => 'AdminMenusController@postFindData', 'as' => 'AdminMenusControllerPostFindData',]);
-    Route::post('menus/action-selected', ['uses' => 'AdminMenusController@postActionSelected', 'as' => 'AdminMenusControllerPostActionSelected',]);
-    //Route::post('menus/upload-summernote', ['uses' => 'AdminMenusController@postUploadSummernote', 'as' => 'AdminMenusControllerPostUploadSummernote',]);
-    //Route::post('menus/upload-file', ['uses' => 'AdminMenusController@postUploadFile', 'as' => 'AdminMenusControllerPostUploadFile',]);
+    $ctrl = 'AdminMenusController';
+    Route::get('menus/', $ctrl.'@getIndex')->name($ctrl.'GetIndex');
+    Route::get('menus/export-data', $ctrl.'@getExportData')->name($ctrl.'GetExportData');
+    Route::post('menus/export-data', $ctrl.'@postExportData')->name($ctrl.'PostExportData');
+    Route::get('menus/import-data', $ctrl.'@getImportData')->name($ctrl.'GetImportData');
+    Route::post('menus/do-import-chunk', $ctrl.'@postDoImportChunk')->name($ctrl.'PostDoImportChunk');
+    Route::get('menus/data-query', $ctrl.'@getDataQuery')->name($ctrl.'GetDataQuery');
+    Route::get('menus/data-table', $ctrl.'@getDataTable')->name($ctrl.'GetDataTable');
+    Route::get('menus/data-modal-datatable', $ctrl.'@getDataModalDatatable')->name($ctrl.'GetDataModalDatatable');
+    Route::get('menus/update-single', $ctrl.'@getUpdateSingle')->name($ctrl.'GetUpdateSingle');
+    Route::get('menus/add', $ctrl.'@getAdd')->name($ctrl.'GetAdd');
+    Route::get('menus/edit/{id?}', $ctrl.'@getEdit')->name($ctrl.'GetEdit');
+    Route::get('menus/delete/{id?}', $ctrl.'@getDelete')->name($ctrl.'GetDelete');
+    Route::get('menus/detail/{id?}', $ctrl.'@getDetail')->name($ctrl.'GetDetail');
+    Route::get('menus/delete-image', $ctrl.'@getDeleteImage')->name($ctrl.'GetDeleteImage');
+    Route::post('menus/save-menu', $ctrl.'@postSaveMenu')->name($ctrl.'PostSaveMenu');
+    Route::post('menus/export-data', $ctrl.'@postExportData')->name($ctrl.'PostExportData');
+    Route::post('menus/add-save', $ctrl.'@postAddSave')->name($ctrl.'PostAddSave');
+    Route::post('menus/edit-save/{id?}', $ctrl.'@postEditSave')->name($ctrl.'PostEditSave');
+    Route::post('menus/find-data', $ctrl.'@postFindData')->name($ctrl.'PostFindData');
+    Route::post('menus/action-selected', $ctrl.'@postActionSelected')->name($ctrl.'PostActionSelected');
 });
 
 if (! Request::is(cbAdminPath())) {
@@ -45,7 +38,10 @@ if (! Request::is(cbAdminPath())) {
 
 $dashboardMenu = DB::table('cms_menus')->where('is_dashboard', 1)->first();
 // ROUTER FOR OWN CONTROLLER FROM CB
-Route::group(['middleware' => ['web', CBBackend::class], 'prefix' => cbAdminPath(), 'namespace' => ctrlNamespace(),
+Route::group([
+    'middleware' => ['web', CBBackend::class],
+    'prefix' => cbAdminPath(),
+    'namespace' => ctrlNamespace(),
 ], function () use ($dashboardMenu) {
 
     if (! $dashboardMenu) {
@@ -53,7 +49,6 @@ Route::group(['middleware' => ['web', CBBackend::class], 'prefix' => cbAdminPath
     }
     $dashboardType = $dashboardMenu->type;
     $path = $dashboardMenu->path;
-
 
     if ($dashboardType == MenuTypes::Statistic) {
         Route::get('/', cbModulesNS('StatisticModule\\AdminStatisticBuilderController@getDashboard'));
@@ -71,7 +66,10 @@ Route::group(['middleware' => ['web', CBBackend::class], 'prefix' => cbAdminPath
     }
 });
 
-Route::group(['middleware' => ['web', CBBackend::class], 'prefix' => cbAdminPath(), 'namespace' => '\crocodicstudio\crudbooster\controllers',
+Route::group([
+    'middleware' => ['web', CBBackend::class],
+    'prefix' => cbAdminPath(),
+    'namespace' => '\crocodicstudio\crudbooster\controllers',
 ], function () use ($dashboardMenu) {
     if (! $dashboardMenu) {
         CRUDBooster::routeController('/', cbModulesNS('AuthModule\AuthController'));
