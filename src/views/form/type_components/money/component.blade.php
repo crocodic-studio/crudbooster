5defbd551cad66e27686d42b1970a454e6e3eb06
imanghafoori <imanghafoori1@gmail.com>
2018-05-11 21:30:27 +0430

    fix

--- src/views/form/type_components/money/component.blade.php
+++ src/views/form/type_components/money/component.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group {{$header_group_class}} {{ ($errors->first($name))?'has-error': '' }}" id="form-group-{{$name}}"
      style="{{@$formInput['style']}}">
-    <label class="control-label col-sm-2">{{$label}} {!!($required)? '<span class="text-danger" title="{!! cbTrans('this_field_is_required') !!}">*</span>': '' !!}</label>
+    <label class='control-label col-sm-2'>{{$label}} {!!($required)?  "<span class='text-danger' title='".cbTrans('this_field_is_required')."'>*</span>":"" !!}</label>
 
-    <div class="{{$col_width?:'col-sm-10'}}">
+    <div class="{{ $col_width ?: 'col-sm-10' }}">
         <input type="text" title="{{$label}}"
                {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} class="form-control inputMoney"
                name="{{$name}}" id="{{$name}}" value="{{$value}}">
