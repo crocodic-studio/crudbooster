2a793978a571dac9b6187a75b2fc7041999985b6
imanghafoori <imanghafoori1@gmail.com>
2018-05-27 03:09:38 +0430

    clean up

--- src/Modules/NotificationsModule/NotificationForm.php
+++ src/Modules/NotificationsModule/NotificationForm.php
@@ -6,11 +6,12 @@ class NotificationForm
 {
     public static function makeForm()
     {
-        $form = [];
-        $form[] = ['label' => "Content", 'name' => "content", 'type' => "text"];
-        $form[] = ['label' => "Icon", 'name' => "icon", 'type' => "text"];
-        $form[] = ['label' => "Notification Command", 'name' => "notification_command", 'type' => "textarea"];
-        $form[] = ['label' => "Is Read", 'name' => "is_read", 'type' => "text"];
-        return $form;
+        //todo: translate
+        return [
+            ['label' => "Content", 'name' => "content", 'type' => "text"],
+            ['label' => "Icon", 'name' => "icon", 'type' => "text"],
+            ['label' => "Notification Command", 'name' => "notification_command", 'type' => "textarea"],
+            ['label' => "Is Read", 'name' => "is_read", 'type' => "text"],
+        ];
     }
 }
