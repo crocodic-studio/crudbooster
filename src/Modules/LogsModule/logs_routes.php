c0c0b85cd73d7aae7000f0deaa6c76a97394ebbb
imanghafoori <imanghafoori1@gmail.com>
2018-05-30 01:14:47 +0430

    remove extra routes

--- src/Modules/LogsModule/logs_routes.php
+++ src/Modules/LogsModule/logs_routes.php
@@ -6,20 +6,21 @@ Route::group([
 ], function () {
     $ctrl = 'AdminLogsController';
     Route::get('logs/', $ctrl.'@getIndex')->name($ctrl.'GetIndex');
-    Route::get('logs/export-data', $ctrl.'@getExportData')->name($ctrl.'GetExportData');
     Route::get('logs/data-query', $ctrl.'@getDataQuery')->name($ctrl.'GetDataQuery');
     Route::get('logs/data-table', $ctrl.'@getDataTable')->name($ctrl.'GetDataTable');
     Route::get('logs/data-modal-datatable/{one?}', $ctrl.'@getDataModalDatatable')->name($ctrl.'GetDataModalDatatable');
     Route::get('logs/update-single/{one?}', $ctrl.'@getUpdateSingle')->name($ctrl.'GetUpdateSingle');
-    Route::get('logs/edit/{id?}', $ctrl.'@getEdit')->name($ctrl.'GetEdit');
-    Route::get('logs/add/{id?}', $ctrl.'@getAdd')->name($ctrl.'GetAdd');
     Route::get('logs/delete/{id?}', $ctrl.'@getDelete')->name($ctrl.'GetDelete');
     Route::get('logs/detail/{id?}', $ctrl.'@getDetail')->name($ctrl.'GetDetail');
-    Route::get('logs/import-data', $ctrl.'@getImportData')->name($ctrl.'GetImportData');
     Route::post('logs/find-data/{one?}', $ctrl.'@postFindData')->name($ctrl.'PostFindData');
-    Route::post('logs/add-save/{one?}', $ctrl.'@postAddSave')->name($ctrl.'PostAddSave');
-    Route::post('logs/do-import-chunk/{one?}', $ctrl.'@postDoImportChunk')->name($ctrl.'PostDoImportChunk');
     Route::post('logs/action-selected/{one?}', $ctrl.'@postActionSelected')->name($ctrl.'PostActionSelected');
-    Route::post('logs/export-data/{one?}', $ctrl.'@postExportData')->name($ctrl.'PostExportData');
-    Route::post('logs/edit-save/{one?}', $ctrl.'@postEditSave')->name($ctrl.'PostEditSave');
+
+    //Route::get('logs/edit/{id?}', $ctrl.'@getEdit')->name($ctrl.'GetEdit');
+    //Route::get('logs/add/{id?}', $ctrl.'@getAdd')->name($ctrl.'GetAdd');
+    //Route::post('logs/add-save/{one?}', $ctrl.'@postAddSave')->name($ctrl.'PostAddSave');
+    //Route::post('logs/do-import-chunk/{one?}', $ctrl.'@postDoImportChunk')->name($ctrl.'PostDoImportChunk');
+    //Route::get('logs/import-data', $ctrl.'@getImportData')->name($ctrl.'GetImportData');
+    //Route::post('logs/export-data/{one?}', $ctrl.'@postExportData')->name($ctrl.'PostExportData');
+    //Route::get('logs/export-data', $ctrl.'@getExportData')->name($ctrl.'GetExportData');
+    //Route::post('logs/edit-save/{one?}', $ctrl.'@postEditSave')->name($ctrl.'PostEditSave');
 });
