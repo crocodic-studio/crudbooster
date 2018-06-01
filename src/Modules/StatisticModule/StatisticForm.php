474993604f3259f5979e6d7374adf74295b30c08
imanghafoori <imanghafoori1@gmail.com>
2018-06-02 00:46:42 +0430

    clean up

--- src/Modules/StatisticModule/StatisticForm.php
+++ src/Modules/StatisticModule/StatisticForm.php
@@ -6,16 +6,15 @@ class StatisticForm
 {
     public static function makeForm()
     {
-        $form = [];
-        $form[] = [
-            "label" => "Name",
-            "name" => "name",
-            "type" => "text",
-            "required" => true,
-            "validation" => "required|min:3|max:255",
-            "placeholder" => "You can only enter the letter only",
+        return [
+            [
+                "label" => "Name",
+                "name" => "name",
+                "type" => "text",
+                "required" => true,
+                "validation" => "required|min:3|max:255",
+                "placeholder" => "You can only enter the letter only",
+            ],
         ];
-
-        return $form;
     }
 }
