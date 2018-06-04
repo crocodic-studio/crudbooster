0a1a45b455465e3363c20280edc96c48e5529a62
imanghafoori <imanghafoori1@gmail.com>
2018-06-05 01:04:03 +0430

    clean up

--- src/Modules/EmailTemplates/EmailTemplateForm.php
+++ src/Modules/EmailTemplates/EmailTemplateForm.php
@@ -6,48 +6,68 @@ class EmailTemplateForm
 {
     public static function makeForm()
     {
-        $form = [];
-        $form[] = [
-            'label' => 'Template Name',
-            'name' => 'name',
-            'type' => 'text',
-            'required' => true,
-            'validation' => 'required|min:3|max:255|alpha_spaces',
-            'placeholder' => 'You can only enter the letter only',
-        ];
-        $form[] = ['label' => 'Slug', 'type' => 'text', 'name' => 'slug', 'required' => true, 'validation' => 'required|unique:cms_email_templates,slug'];
-        $form[] = ['label' => 'Subject', 'name' => 'subject', 'type' => 'text', 'required' => true, 'validation' => 'required|min:3|max:255'];
-        $form[] = ['label' => 'Content', 'name' => 'content', 'type' => 'wysiwyg', 'required' => true, 'validation' => 'required'];
-        $form[] = ['label' => 'Description', 'name' => 'description', 'type' => 'text', 'required' => true, 'validation' => 'required|min:3|max:255'];
+        $form = [
+            [
+                'label' => 'Template Name',
+                'name' => 'name',
+                'type' => 'text',
+                'required' => true,
+                'validation' => 'required|min:3|max:255|alpha_spaces',
+                'placeholder' => 'You can only enter the letter only',
+            ],
+            [
+                'label' => 'Slug',
+                'type' => 'text',
+                'name' => 'slug',
+                'required' => true,
+                'validation' => 'required|unique:cms_email_templates,slug'],
+            [
+                'label' => 'Subject',
+                'name' => 'subject',
+                'type' => 'text',
+                'required' => true,
+                'validation' => 'required|min:3|max:255'],
+            [
+                'label' => 'Content',
+                'name' => 'content',
+                'type' => 'wysiwyg',
+                'required' => true,
+                'validation' => 'required'],
+            [
+                'label' => 'Description',
+                'name' => 'description',
+                'type' => 'text',
+                'required' => true,
+                'validation' => 'required|min:3|max:255'],
 
-        $form[] = [
-            'label' => 'From Name',
-            'name' => 'from_name',
-            'type' => 'text',
-            'required' => false,
-            'width' => 'col-sm-6',
-            'placeholder' => 'Optional',
-        ];
-        $form[] = [
-            'label' => 'From Email',
-            'name' => 'from_email',
-            'type' => 'email',
-            'required' => false,
-            'validation' => 'email',
-            'width' => 'col-sm-6',
-            'placeholder' => 'Optional',
-        ];
+            [
+                'label' => 'From Name',
+                'name' => 'from_name',
+                'type' => 'text',
+                'required' => false,
+                'width' => 'col-sm-6',
+                'placeholder' => 'Optional',
+            ],
+            [
+                'label' => 'From Email',
+                'name' => 'from_email',
+                'type' => 'email',
+                'required' => false,
+                'validation' => 'email',
+                'width' => 'col-sm-6',
+                'placeholder' => 'Optional',
+            ],
 
-        $form[] = [
-            'label' => 'Cc Email',
-            'name' => 'cc_email',
-            'type' => 'email',
-            'required' => false,
-            'validation' => 'email',
-            'placeholder' => 'Optional',
+            [
+                'label' => 'Cc Email',
+                'name' => 'cc_email',
+                'type' => 'email',
+                'required' => false,
+                'validation' => 'email',
+                'placeholder' => 'Optional',
+            ],
         ];
 
         return $form;
     }
-
 }
