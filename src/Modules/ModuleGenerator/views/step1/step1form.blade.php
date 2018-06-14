9eecb855bb054eb87bb24e8d4e72044fa9cefcf1
imanghafoori <imanghafoori1@gmail.com>
2018-06-15 04:17:25 +0430

    fix

--- src/Modules/ModuleGenerator/views/step1/step1form.blade.php
+++ src/Modules/ModuleGenerator/views/step1/step1form.blade.php
@@ -1,18 +1,22 @@
-<form method="post" action="{{route('AdminModulesControllerPostStep2')}}">
+<?php
+$module = CRUDBooster::getCurrentModule();
+
+?>
+<form method="post" action="{{route('AdminModulesControllerPostStep1')}}">
 
     <div class="box-body">
 
         <input type="hidden" name="_token" value="{{csrf_token()}}">
-        <input type="hidden" name="id" value="{{$row->id}}">
+        <input type="hidden" name="id" value="{{$module->id}}">
 
         <div class="row">
             <div class="col-md-12 form-group">
                 <label for="">Table</label>
-                <select name="table" id="table" required class="select2 form-control" value="{{$row->table_name}}">
+                <select name="table" id="table" required class="select2 form-control" value="{{$module->table_name}}">
                     <option value="">{{cbTrans('text_prefix_option')}} Table</option>
 
                     @foreach(CRUDBooster::listCbTables() as $table)
-                        <option {{($table == $row->table_name)?"selected":""}} value="{{$table}}">{{$table}}</option>
+                        <option {{($table == $module->table_name)?"selected":""}} value="{{$table}}">{{$table}}</option>
                     @endforeach
 
                 </select>
@@ -26,7 +30,7 @@
 
             <div class="col-md-12 form-group">
                 <label for="">Module Name</label>
-                <input type="text" class="form-control" required name="name" value="{{$row->name}}">
+                <input type="text" class="form-control" required name="name" value="{{$module->name}}">
             </div>
         </div>
         <div class="row">
@@ -37,7 +41,7 @@
                         $fontAwesome = \crocodicstudio\crudbooster\controllers\Helpers\FontAwesome::cssClass();
                     @endphp
                     @foreach($fontAwesome as $font)
-                        <option {{($row->icon == 'fa fa-'.$font)?"selected":""}} value="fa fa-{{$font}}">{{$font}}</option>
+                        <option {{($module->icon == 'fa fa-'.$font)?"selected":""}} value="fa fa-{{$font}}">{{$font}}</option>
                     @endforeach
                 </select>
             </div>
@@ -45,7 +49,7 @@
         <div class="row">
             <div class="col-md-12 form-group">
                 <label for="">Module Slug</label>
-                <input type="text" class="form-control" required name="path" value="{{$row->path}}">
+                <input type="text" class="form-control" required name="path" value="{{$module->path}}">
                 <div class="help-block">
                     Please alpha numeric only, without space instead _ and or special character
                 </div>
