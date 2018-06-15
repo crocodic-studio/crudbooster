81cc5ddf000052c06fb49cc657ea46fbd01d09f5
Adrian Lim <adrian.lbs@gmail.com>
2018-06-15 14:03:11 +0800

    Added logging for error checking

--- src/controllers/CBController.php
+++ src/controllers/CBController.php
@@ -1589,7 +1589,7 @@ class CBController extends Controller {
 			$a = array();
 			foreach($select_column as $sk => $s) {
 				$colname = $table_columns[$sk];
-				//Log::error($colname);
+				Log::error($colname);
 
 				/*if(CRUDBooster::isForeignKey($colname)) {
 
@@ -1639,7 +1639,7 @@ class CBController extends Controller {
 			}
 			/*if ($f != FALSE)
 				array_filter($linksArray, function($value) { return $value[''] !== ''; });*/
-			//Log::error($a);
+			Log::error($a);
 
 			$has_title_field = true;
 			foreach($a as $k=>$v) {
