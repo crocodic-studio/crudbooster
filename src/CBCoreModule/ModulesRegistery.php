af24e54bdf843beb815f76f73e277e36c84a547b
imanghafoori <imanghafoori1@gmail.com>
2018-06-16 12:11:40 +0430

    prevent module overrides

--- src/CBCoreModule/ModulesRegistery.php
+++ src/CBCoreModule/ModulesRegistery.php
@@ -8,7 +8,11 @@ class ModulesRegistery
 
     public function addModule($key, $module)
     {
+        if (array_key_exists($key, $this->modules)) {
+            throw new \Exception("$key already exists.");
+        }
         $this->modules[$key] = $module;
+        return $this->modules;
     }
 
     public function getModule($key)
