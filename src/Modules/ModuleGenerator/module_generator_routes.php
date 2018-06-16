9d3e3170658002184d8e185ac56557727eee6b00
imanghafoori <imanghafoori1@gmail.com>
2018-06-16 14:06:47 +0430

    fix

--- src/Modules/ModuleGenerator/module_generator_routes.php
+++ src/Modules/ModuleGenerator/module_generator_routes.php
@@ -7,47 +7,36 @@ Route::group([
     'prefix' => cbAdminPath(),
     'namespace' => $namespace,
 ], function () use ($namespace) {
-    Route::get('modules/', ['uses' => 'AdminModulesController@getIndex', 'as' => 'AdminModulesControllerGetIndex']);
-    Route::get('modules/test', ['uses' => 'AdminModulesController@getTest', 'as' => 'AdminModulesControllerGetTest']);
-    Route::get('modules/export-data', ['uses' => 'AdminModulesController@getExportData', 'as' => 'AdminModulesControllerGetExportData']);
-    Route::get('modules/table-columns/{table?}', ['uses' => 'AdminModulesController@getTableColumns', 'as' => 'AdminModulesControllerGetTableColumns']);
-    Route::get('modules/check-slug/{slug?}', ['uses' => 'AdminModulesController@getCheckSlug', 'as' => 'AdminModulesControllerGetCheckSlug']);
-    Route::get('modules/add', ['uses' => 'AdminModulesController@getAdd', 'as' => 'AdminModulesControllerGetAdd']);
+    Route::get('modules/', 'AdminModulesController@getIndex')->name('AdminModulesControllerGetIndex');
+    Route::get('modules/table-columns/{table?}', 'AdminModulesController@getTableColumns')->name('AdminModulesControllerGetTableColumns');
+    Route::get('modules/check-slug/{slug?}', 'AdminModulesController@getCheckSlug')->name('AdminModulesControllerGetCheckSlug');
+    Route::get('modules/add', 'AdminModulesController@getAdd')->name('AdminModulesControllerGetAdd');
 
-    Route::get('modules/step1/{id?}', ['uses' => 'AdminModulesController@getStep1', 'as' => 'AdminModulesControllerGetStep1']);
-    Route::post('modules/step1', ['uses' => 'AdminModulesController@postStep1', 'as' => 'AdminModulesControllerPostStep1',]);
+    Route::get('modules/step1/{id?}', 'AdminModulesController@getStep1')->name('AdminModulesControllerGetStep1');
+    Route::post('modules/step1', 'AdminModulesController@postStep1')->name('AdminModulesControllerPostStep1');
 
-    Route::get('modules/step2/{id?}', ['uses' => 'AdminModulesController@getStep2', 'as' => 'AdminModulesControllerGetStep2']);
-    Route::post('modules/step3', ['uses' => 'AdminModulesController@postStep2', 'as' => 'AdminModulesControllerpostStep2',]);
+    Route::get('modules/step2/{id?}', 'AdminModulesController@getStep2')->name('AdminModulesControllerGetStep2');
+    Route::post('modules/step3', 'AdminModulesController@postStep2')->name('AdminModulesControllerPostStep2');
 
-    Route::get('modules/step3/{id?}', ['uses' => 'AdminModulesController@getStep3', 'as' => 'AdminModulesControllerGetStep3']);
-    Route::post('modules/step4', ['uses' => 'AdminModulesController@postStep3', 'as' => 'AdminModulesControllerpostStep3',]);
+    Route::get('modules/step3/{id?}', 'AdminModulesController@getStep3')->name('AdminModulesControllerGetStep3');
+    Route::post('modules/step4', 'AdminModulesController@postStep3')->name('AdminModulesControllerPostStep3');
 
-    Route::get('modules/type-info/{type?}', ['uses' => 'AdminModulesController@getTypeInfo', 'as' => 'AdminModulesControllerGetTypeInfo']);
+    Route::get('modules/type-info/{type?}', 'AdminModulesController@getTypeInfo')->name('AdminModulesControllerGetTypeInfo');
 
-    Route::get('modules/step4/{id?}', ['uses' => 'AdminModulesController@getStep4', 'as' => 'AdminModulesControllerGetStep4']);
-    Route::post('modules/step-finish', ['uses' => 'AdminModulesController@postStepFinish', 'as' => 'AdminModulesControllerPostStepFinish',]);
+    Route::get('modules/step4/{id?}', 'AdminModulesController@getStep4')->name('AdminModulesControllerGetStep4');
+    Route::post('modules/step-finish', 'AdminModulesController@postStepFinish')->name('AdminModulesControllerPostStepFinish');
 
-    Route::get('modules/delete-image', ['uses' => 'AdminModulesController@getDeleteImage', 'as' => 'AdminModulesControllerGetDeleteImage']);
-    Route::get('modules/data-query', ['uses' => 'AdminModulesController@getDataQuery', 'as' => 'AdminModulesControllerGetDataQuery']);
-    Route::get('modules/data-table', ['uses' => 'AdminModulesController@getDataTable', 'as' => 'AdminModulesControllerGetDataTable']);
-    Route::get('modules/data-modal-datatable', ['uses' => 'AdminModulesController@getDataModalDatatable', 'as' => 'AdminModulesControllerGetDataModalDatatable']);
-    Route::get('modules/update-single', ['uses' => 'AdminModulesController@getUpdateSingle', 'as' => 'AdminModulesControllerGetUpdateSingle']);
-    Route::get('modules/edit/{id?}', ['uses' => 'AdminModulesController@getEdit', 'as' => 'AdminModulesControllerGetEdit']);
-    Route::get('modules/delete/{id?}', ['uses' => 'AdminModulesController@getDelete', 'as' => 'AdminModulesControllerGetDelete']);
-    Route::get('modules/detail/{id?}', ['uses' => 'AdminModulesController@getDetail', 'as' => 'AdminModulesControllerGetDetail']);
-    Route::get('modules/import-data', ['uses' => 'AdminModulesController@getImportData', 'as' => 'AdminModulesControllerGetImportData']);
+    Route::get('modules/delete-image', 'AdminModulesController@getDeleteImage')->name('AdminModulesControllerGetDeleteImage');
+    Route::get('modules/data-query', 'AdminModulesController@getDataQuery')->name('AdminModulesControllerGetDataQuery');
+    Route::get('modules/data-table', 'AdminModulesController@getDataTable')->name('AdminModulesControllerGetDataTable');
+    Route::get('modules/data-modal-datatable', 'AdminModulesController@getDataModalDatatable')->name('AdminModulesControllerGetDataModalDatatable');
+    Route::get('modules/update-single', 'AdminModulesController@getUpdateSingle')->name('AdminModulesControllerGetUpdateSingle');
+    Route::get('modules/edit/{id?}', 'AdminModulesController@getEdit')->name('AdminModulesControllerGetEdit');
+    Route::get('modules/delete/{id?}', 'AdminModulesController@getDelete')->name('AdminModulesControllerGetDelete');
+    Route::get('modules/detail/{id?}', 'AdminModulesController@getDetail')->name('AdminModulesControllerGetDetail');
 
-    Route::post('modules/add-save', ['uses' => 'AdminModulesController@postAddSave', 'as' => 'AdminModulesControllerPostAddSave',]);
-    Route::post('modules/edit-save/{one?}', ['uses' => 'AdminModulesController@postEditSave', 'as' => 'AdminModulesControllerPostEditSave',]);
-    Route::post('modules/export-data', ['uses' => 'AdminModulesController@postExportData', 'as' => 'AdminModulesControllerPostExportData',]);
-    Route::post('modules/find-data', ['uses' => 'AdminModulesController@postFindData', 'as' => 'AdminModulesControllerPostFindData',]);
-
-    //Route::post('modules/done-import', ['uses' => 'AdminModulesController@postDoneImport', 'as' => 'AdminModulesControllerPostDoneImport',]);
-
-    Route::post('modules/do-import-chunk', ['uses' => 'AdminModulesController@postDoImportChunk', 'as' => 'AdminModulesControllerPostDoImportChunk',]);
-    //Route::post('modules/do-upload-import-data', ['uses' => 'AdminModulesController@postDoUploadImportData', 'as' => 'AdminModulesControllerPostDoUploadImportData',]);
-    Route::post('modules/action-selected', ['uses' => 'AdminModulesController@postActionSelected', 'as' => 'AdminModulesControllerPostActionSelected',]);
-    //Route::post('modules/upload-summernote', ['uses' => 'AdminModulesController@postUploadSummernote', 'as' => 'AdminModulesControllerPostUploadSummernote',]);
-    //Route::post('modules/upload-file', ['uses' => 'AdminModulesController@postUploadFile', 'as' => 'AdminModulesControllerPostUploadFile',]);
+    Route::post('modules/add-save', 'AdminModulesController@postAddSave')->name('AdminModulesControllerPostAddSave');
+    Route::post('modules/edit-save/{one?}', 'AdminModulesController@postEditSave')->name('AdminModulesControllerPostEditSave');
+    Route::post('modules/find-data', 'AdminModulesController@postFindData')->name('AdminModulesControllerPostFindData');
+    Route::post('modules/action-selected', 'AdminModulesController@postActionSelected')->name('AdminModulesControllerPostActionSelected');
 });
