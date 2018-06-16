2043dd87bdcbfd80fcc04957d3c05b274e84f679
imanghafoori <imanghafoori1@gmail.com>
2018-06-16 21:24:57 +0430

    fix

--- src/views/form/type_components/wysiwyg/component.blade.php
+++ src/views/form/type_components/wysiwyg/component.blade.php
@@ -2,7 +2,7 @@
     <script type="text/javascript">
         $(document).ready(function () {
             $('#textarea_{{$name}}').summernote({
-                height: {{ ($formInput['options']['height'])?:"($(window).height() - 300)"}},
+                height: {{ array_get($formInput, 'options.height')?:"($(window).height() - 300)"}},
                 callbacks: {
                     onImageUpload: function (image) {
                         uploadImage{{$name}}(image[0]);
