33a3274e49970d9e3ec3260bad1ba2a0f66703e4
aminurizam <>
2018-07-10 07:33:50 +0000

    test-hide form

--- src/views/default/type_components/child/component.blade.php
+++ src/views/default/type_components/child/component.blade.php
@@ -17,7 +17,7 @@
 		</div>
 		<div class="panel-body">
 			
-			<div class='row'>
+			{{-- <div class='row'>
 				<div class='col-sm-10'>
 					<div class="panel panel-default">
 						<div class="panel-heading"><i class="fa fa-pencil-square-o"></i> Form</div>
@@ -468,7 +468,7 @@
 						</div>
 					</div>
 				</div>
-			</div>
+			</div> --}}
 
 			<div class="panel panel-default">
 				<div class="panel-heading">
