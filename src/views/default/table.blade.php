0ef197daafb88e0cf4dd552385326a5b2481db3b
Adrian Lim <adrian.lbs@gmail.com>
2018-07-12 03:06:52 +0800

    Added options list

--- src/views/default/table.blade.php
+++ src/views/default/table.blade.php
@@ -350,10 +350,9 @@
                             @if($col["type_form"]=='select')
                               <select disabled class='filter-value form-control' name='filter_column[{{$col["field_with"]}}][value]' disabled>
                                 <option value=""></option>
-                                <option value="volvo">Volvo</option>
-                                <option value="saab">Saab</option>
-                                <option value="mercedes">Mercedes</option>
-                                <option value="audi">Audi</option>
+                                @foreach ($col["optionlist"] as $opt)
+                                  <option value="{{$opt}}">{{$opt}}</option>                                  
+                                @endforeach
                               </select>                            
                             @else
                               <input type='text' class='filter-value form-control' style="{{ (CRUDBooster::getTypeFilter($col["field_with"]) == 'between')?"display:none":"display:block"}}" disabled name='filter_column[{{$col["field_with"]}}][value]' value='{{ (!is_array(CRUDBooster::getValueFilter($col["field_with"])))?CRUDBooster::getValueFilter($col["field_with"]):"" }}'>
