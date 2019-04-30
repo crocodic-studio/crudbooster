89eb970119eb965ebf7a5fea2a71a9589b179df8
rajakodings <rajakodings@gmail.com>
2019-04-30 08:45:31 +0700

    Fixing

--- src/views/layouts/layout_login.blade.php
+++ src/views/layouts/layout_login.blade.php
@@ -5,9 +5,7 @@
     <title>{{trans("crudbooster.page_title_login")}} : {{ cb()->getAppName() }}</title>
     <meta name='generator' content='CRUDBooster'/>
     <meta name='robots' content='noindex,nofollow'/>
-    <link rel="shortcut icon"
-          href="{{ getSetting('favicon')?asset(getSetting('favicon')):cbAsset('logo.png') }}">
-
+    <link rel="shortcut icon" href="{{ getSetting('favicon')?asset(getSetting('favicon')):cbAsset('logo.png') }}">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
     <link href="{{cbAsset('adminlte/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
@@ -15,21 +13,20 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
     <!-- Theme style -->
     <link href="{{cbAsset('adminlte/dist/css/AdminLTE.min.css')}}" rel="stylesheet" type="text/css"/>
-
     <!-- support rtl-->
     @if (in_array(App::getLocale(), ['ar', 'fa']))
         <link rel="stylesheet" href="//cdn.rawgit.com/morteza/bootstrap-rtl/v3.3.4/dist/css/bootstrap-rtl.min.css">
-        <link href="{{ asset("vendor/crudbooster/assets/rtl.css")}}" rel="stylesheet" type="text/css"/>
-@endif
+        <link href="{{ cbAsset("rtl.css")}}" rel="stylesheet" type="text/css"/>
+    @endif
 
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
 
-    <link rel='stylesheet' href='{{asset("vendor/crudbooster/assets/css/main.css")}}'/>
+    <link rel='stylesheet' href='{{ cbAsset("css/main.css")}}'/>
     <style type="text/css">
         .login-page {
             background: #dddddd;
@@ -60,9 +57,7 @@
 <div class="login-box">
     <div class="login-logo">
         <a href="{{ cb()->getLoginUrl() }}">
-            <img title='{{ cb()->getAppName() }}'
-                 src='{{ getSetting("login_logo")?asset(getSetting('login_logo')):cbAsset('logo.png') }}'
-                 style='max-width: 100%;max-height:170px'/>
+            <img title='{{ cb()->getAppName() }}' src='{{ getSetting("login_logo")?asset(getSetting('login_logo')):cbAsset('logo.png') }}' style='max-width: 100%;max-height:170px'/>
         </a>
     </div><!-- /.login-logo -->
     <div class="login-box-body">
