1badbfa56113e9126a8fc8aac462313279b8ff11
rajakodings <rajakodings@gmail.com>
2019-09-23 23:04:36 +0700

    Update & Improvement

--- src/database/migrations/2016_08_07_152421_modify_users.php
+++ src/database/migrations/2016_08_07_152421_modify_users.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddRoleUsers extends Migration
+class ModifyUsers extends Migration
 {
     /**
      * Run the migrations.
