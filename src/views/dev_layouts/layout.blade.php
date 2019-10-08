9faf513e192f9c0a66c8eb1d059a718fa3ca8e9b
rajakodings <rajakodings@gmail.com>
2019-10-08 17:27:55 +0700

    Update & Improvement

--- src/views/dev_layouts/layout.blade.php
+++ src/views/dev_layouts/layout.blade.php
@@ -12,9 +12,9 @@
     <meta name='robots' content='noindex,nofollow'/>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
-    <link href="{{cbAsset('adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{cbAsset('js/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
     <!-- Font Awesome Icons -->
-    <link href="{{cbAsset("adminlte/bower_components/font-awesome/css/font-awesome.min.css")}}" rel="stylesheet" type="text/css"/>
+    <link href="{{cbAsset("js/font-awesome/css/font-awesome.min.css")}}" rel="stylesheet" type="text/css"/>
 
     <!-- Theme style -->
     <link href="{{ cbAsset("adminlte/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css"/>
