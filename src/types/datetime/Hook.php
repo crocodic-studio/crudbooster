ffd84054d0529ba548a1577a72ca04b8b201afcd
John Rallis <rallisf1@yahoo.gr>
2020-03-04 13:42:02 +0200

    bugfix: Save datetime with custom format

--- src/types/datetime/Hook.php
+++ src/types/datetime/Hook.php
@@ -27,6 +27,15 @@ class Hook extends TypesHook
         }
     }
 
+    public function assignment($value, $column)
+    {
+		if($column->getFormat()) {
+			return date_create_from_format($column->getFormat(), $value)->format('Y-m-d H:i:s');
+		} else {
+			return $value;
+		}
+    }
+    
     public function detailRender($row, $column)
     {
         return $this->indexRender($row, $column);
