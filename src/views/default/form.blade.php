df702c346700504785cf5b9ac97390ccc33968e7
Renzo Castillo <renzo.castillo@quaira.com>
2020-04-06 15:27:09 -0500

    form detail can receive label through url

--- src/views/default/form.blade.php
+++ src/views/default/form.blade.php
@@ -5,8 +5,8 @@
 
         @if(CRUDBooster::getCurrentMethod() != 'getProfile' && $button_cancel)
             @if(g('return_url'))
-                <p><a title='Return' href='{{g("return_url")}}'><i class='fa fa-chevron-circle-left '></i>
-                        &nbsp; {{trans("crudbooster.form_back_to_list",['module'=>CRUDBooster::getCurrentModule()->name])}}</a></p>
+                <p><a title='Return' href='{{urldecode(g("return_url"))}}'><i class='fa fa-chevron-circle-left '></i>
+                        &nbsp; {{trans("crudbooster.form_back_to_list",['module'=>g('label') ? :CRUDBooster::getCurrentModule()->name])}}</a></p>
             @else
                 <p><a title='Main Module' href='{{CRUDBooster::mainpath()}}'><i class='fa fa-chevron-circle-left '></i>
                         &nbsp; {{trans("crudbooster.form_back_to_list",['module'=>CRUDBooster::getCurrentModule()->name])}}</a></p>
