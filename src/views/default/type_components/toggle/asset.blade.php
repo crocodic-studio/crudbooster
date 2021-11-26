c690a2cf6c05eca86b5826b1ca1412011d695ba6
Renzo <renzo.castillo@quaira.com>
2021-11-26 14:41:01 -0500

    toggle component
    new component to adding nice UI for boolean data type

---
+++ src/views/default/type_components/toggle/asset.blade.php
@@ -0,0 +1,6 @@
+@push('head')
+    <link rel='stylesheet' href='<?php echo asset("vendor/crudbooster/assets/bootstrap-toggle/css/bootstrap-toggle.min.css")?>'/>
+@endpush
+@push('bottom')
+    <script src='<?php echo asset("vendor/crudbooster/assets/bootstrap-toggle/js/bootstrap-toggle.min.js")?>'></script>
+@endpush
---
+++ src/views/default/type_components/toggle/component.blade.php
@@ -0,0 +1,36 @@
+<div class='form-group {{$header_group_class}} {{ ($errors->first($name))?"has-error":"" }}' id='form-group-{{$name}}'
+     style="{{@$form['style']}}">
+    <label class='control-label col-sm-2'>
+        {{$form['label']}}
+        @if($required)
+            <span class='text-danger' title='{!! cbLang('this_field_is_required') !!}'>*</span>
+        @endif
+    </label>
+
+    <div class="{{$col_width?:'col-sm-10'}}">
+        <input type='checkbox' data-toggle="toggle" data-size="small" title="{{$form['label']}}"
+               {{$required}} {{$readonly}} {!!$placeholder!!} {{$disabled}} {{$validation['max']?"maxlength=".$validation['max']:""}} class='form-control checkboxtoggle'
+               name="{{$name}}" id="{{$name}}" value='{{$value ? "1" : "0"}}' @if($value) checked
+        @else @endif/>
+
+        <div
+            class="text-danger">{!! $errors->first($name)?"<i class='fa fa-info-circle'></i> ".$errors->first($name):"" !!}</div>
+        <p class='help-block'>{{ @$form['help'] }}</p>
+
+    </div>
+</div>
+
+@push('bottom')
+    <script type="text/javascript">
+        if ($.fn.bootstrapToggle) {
+            $('.checkboxtoggle').bootstrapToggle({
+                on: 'SI',
+                off: 'NO'
+            });
+        }
+        $(document).on('change', '.checkboxtoggle', function () {
+            let input = $(this);
+            input.val(input.prop('checked') ? 1 : 0);
+        });
+    </script>
+@endpush
